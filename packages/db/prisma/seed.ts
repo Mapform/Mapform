3a1ca76468fa56fd6d723b8006656e858df60688
Nicholas Haley <hello@nichaley.com>
2024-03-04 22:27:50 -0500

    Create db package

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,16 @@
+import { randomBytes } from "node:crypto";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
