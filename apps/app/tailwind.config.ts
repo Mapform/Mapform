7eda8f0827257b7a1bb3abed1aad55969ba3cfe1
Nicholas Haley <hello@nichaley.com>
2024-03-09 11:39:37 -0500

    Get Tailwind wired up

---
+++ apps/app/tailwind.config.ts
@@ -0,0 +1,8 @@
+import sharedConfig from "@mapform/tailwind";
+
+const config = {
+  presets: [sharedConfig],
+  content: ["./src/**/*.{ts,tsx}"],
+};
+
+export default config;
---
+++ packages/tailwind/tailwind.config.ts
@@ -0,0 +1,6 @@
+const config = {
+  content: ["**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
+
+export default config;
