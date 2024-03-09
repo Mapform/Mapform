9320f24b1328703a1a2e98b9a68feef8bc82841e
Nicholas Haley <hello@nichaley.com>
2024-03-09 12:28:35 -0500

    Add basic Clerk auth back

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@mapform/ui";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <UserButton />
       <Button appName="test">test</Button>
     </main>
   );
