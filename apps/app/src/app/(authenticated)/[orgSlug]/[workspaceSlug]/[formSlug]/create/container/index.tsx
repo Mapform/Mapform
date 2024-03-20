85fe0313ca4a7e730af1830b27f8ed8c82d79e06
Nicholas Haley <hello@nichaley.com>
2024-03-20 17:52:04 -0400

    Make map controlled

--- apps/app/src/app/(authenticated)/[orgSlug]/[workspaceSlug]/[formSlug]/create/container/index.tsx
+++ apps/app/src/app/(authenticated)/[orgSlug]/[workspaceSlug]/[formSlug]/create/container/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
 import { MapForm } from "@mapform/mapform";
 import { Button } from "@mapform/ui/components/button";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { env } from "~/env.mjs";
 import type { FormType } from "../actions";
 import { createStep } from "../actions";
@@ -14,18 +15,51 @@ const initialViewState = {
   zoom: 14,
   bearing: 0,
   pitch: 0,
+  padding: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
 };
 
 export function Container({ form }: { form: NonNullable<FormType> }) {
+  const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const createStepWithFromId = createStep.bind(null, form.id, initialViewState);
   const s = searchParams.get("s");
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  useEffect(() => {
+    if (form.steps[0] && !s) {
+      router.push(`${pathname}?${createQueryString("s", form.steps[0].id)}`);
+    }
+  }, [s, form.steps, pathname, router, createQueryString]);
+
+  const currentStep = form.steps.find((step) => step.id === s);
 
   return (
     <div className="flex flex-1">
       <div className="flex flex-col flex-1">
         <div className="h-[500px] w-full p-4 bg-slate-100">
-          <MapForm mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN} />
+          <MapForm
+            mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+            setViewState={(evt) => {
+              setViewState(evt.viewState);
+            }}
+            viewState={viewState}
+          />
         </div>
         <div className="border-t">
           <form action={createStepWithFromId}>
--- packages/mapform/src/MapForm/index.tsx
+++ packages/mapform/src/MapForm/index.tsx
@@ -1,34 +1,30 @@
 "use client";
 
-import Map from "react-map-gl";
+import Map, { ViewState, ViewStateChangeEvent } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Tiptap } from "./TipTap";
 
 interface MapFormProps {
   mapboxAccessToken: string;
-  // initialViewState: {
-  //   longitude: number;
-  //   latitude: number;
-  //   zoom: number;
-  // };
+  viewState: ViewState;
+  setViewState: (viewState: ViewStateChangeEvent) => void;
 }
 
-export function MapForm({ mapboxAccessToken }: MapFormProps) {
+export function MapForm({
+  mapboxAccessToken,
+  viewState,
+  setViewState,
+}: MapFormProps) {
   return (
     <div className="flex w-full h-full">
       <div className="w-64 flex-shrink-0 bg-background">
         <Tiptap />
       </div>
       <Map
-        mapboxAccessToken={mapboxAccessToken}
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14,
-          bearing: 0,
-          pitch: 0,
-        }}
+        {...viewState}
         style={{ flex: 1 }}
+        onMove={setViewState}
+        mapboxAccessToken={mapboxAccessToken}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       />
     </div>
