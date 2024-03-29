c9f7daaee538a6a4dfdefc120a7cc8eaac13f30a
Nicholas Haley <hello@nichaley.com>
2024-03-29 00:01:28 -0400

    define draft / published state, and multi-table inheritance pattern for steps

--- apps/app/src/app/(authenticated)/[orgSlug]/[workspaceSlug]/[formSlug]/create/container/index.tsx
+++ apps/app/src/app/(authenticated)/[orgSlug]/[workspaceSlug]/[formSlug]/create/container/index.tsx
@@ -68,7 +68,7 @@ export function Container({
   return (
     <div className="flex flex-1">
       <div className="flex flex-col flex-1">
-        <div className="h-[500px] w-full p-4 bg-slate-100">
+        <div className="h-[500px] w-full p-4 bg-slate-100 inset-0  bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
           <div className="shadow h-full w-full rounded-md overflow-hidden">
             <MapForm
               mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
