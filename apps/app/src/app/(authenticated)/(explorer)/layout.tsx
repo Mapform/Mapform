0adf90c579fb6514881b95630027825af30ee950
Nicholas Haley <hello@nichaley.com>
2024-05-19 11:35:47 -0400

    wip

--- apps/app/src/app/(authenticated)/(explorer)/layout.tsx
+++ apps/app/src/app/(authenticated)/(explorer)/layout.tsx
@@ -24,7 +24,11 @@ export default async function Layout({
     include: {
       organizationMemberships: {
         include: {
-          organization: true,
+          organization: {
+            include: {
+              workspaces: true,
+            },
+          },
         },
       },
     },
@@ -63,6 +67,33 @@ export default async function Layout({
                     {membership.organization.name}
                   </span>
                 </Link>
+                <ul className="-mx-2 mt-2 space-y-1">
+                  {membership.organization.workspaces.map((workspace) => (
+                    <li key={workspace.id}>
+                      <Link
+                        className={cn(
+                          pathname === workspace.slug
+                            ? "bg-gray-50 text-indigo-600"
+                            : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        )}
+                        href={workspace.slug}
+                      >
+                        <span
+                          className={cn(
+                            pathname === workspace.slug
+                              ? "text-indigo-600 border-indigo-600"
+                              : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                            "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                          )}
+                        >
+                          {workspace.name[0]}
+                        </span>
+                        <span className="truncate">{workspace.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
             ))}
           </ul>
