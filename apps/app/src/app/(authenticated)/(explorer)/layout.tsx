f04dc09ac7051dfbb168840f4654e428d3115f70
Nicholas Haley <hello@nichaley.com>
2024-05-19 11:30:37 -0400

    Get Clerk seeder working

--- apps/app/src/app/(authenticated)/(explorer)/layout.tsx
+++ apps/app/src/app/(authenticated)/(explorer)/layout.tsx
@@ -1,6 +1,7 @@
+import { prisma } from "@mapform/db";
 import { headers } from "next/headers";
 import { cn } from "@mapform/lib/classnames";
-import { clerkClient } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default async function Layout({
@@ -10,7 +11,26 @@ export default async function Layout({
 }) {
   const header = headers();
   const pathname = header.get("next-url");
-  const orgs = await clerkClient.organizations.getOrganizationList();
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const userOrgs = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      organizationMemberships: {
+        include: {
+          organization: true,
+        },
+      },
+    },
+  });
+
+  console.log(111111, userId, userOrgs);
 
   return (
     <div className="flex flex-1">
@@ -18,28 +38,30 @@ export default async function Layout({
       <div className="flex flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2 w-[300px]">
         <nav className="flex flex-1 flex-col">
           <ul className="-mx-2 mt-2 space-y-1">
-            {orgs.map((org) => (
-              <li key={org.id}>
+            {userOrgs?.organizationMemberships.map((membership) => (
+              <li key={membership.organization.id}>
                 <Link
                   className={cn(
-                    pathname === org.slug
+                    pathname === membership.organization.slug
                       ? "bg-gray-50 text-indigo-600"
                       : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                   )}
-                  href={org.slug}
+                  href={membership.organization.slug}
                 >
                   <span
                     className={cn(
-                      pathname === org.slug
+                      pathname === membership.organization.slug
                         ? "text-indigo-600 border-indigo-600"
                         : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
                       "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
                     )}
                   >
-                    {org.name[0]}
+                    {membership.organization.name[0]}
                   </span>
-                  <span className="truncate">{org.name}</span>
+                  <span className="truncate">
+                    {membership.organization.name}
+                  </span>
                 </Link>
               </li>
             ))}
--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,46 +1,152 @@
+import type { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { UserCreateInputSchema } from "./zod";
+import { OrganizationCreateInputSchema } from "./zod";
+import { OrganizationMembershipCreateInputSchema } from "./zod";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const organization = await prisma.organization.create({
-    data: {
-      // Hard coding from Clerk (for now)
-      id: "org_2donNxW45zFE3Ys6qQonHFj0qUc",
-      name: "Mapform",
-      slug: "mapform",
-    },
-  });
+async function createUsersFromClerk() {
+  const users = await clerkClient.users.getUserList();
 
-  const user = await prisma.user.create({
-    data: {
-      // Hard coding from Clerk (for now)
-      id: "user_2domNcdN4yYTRGtuSudlI1I5U45",
-      firstName: "Nic",
-      lastName: "Haley",
-      email: "nicholaswilliamhaley@gmail.com",
-      imageUrl:
-        "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18yZEg5dmg2Mlp4RDhISU5ZZTVWeUhidzcxcFEiLCJyaWQiOiJ1c2VyXzJkb21OY2RONHlZVFJHdHVTdWRsSTFJNVU0NSIsImluaXRpYWxzIjoiTkgifQ",
-    },
-  });
+  await prisma.$transaction([
+    ...users.data.map((user) => {
+      const input: Partial<z.infer<typeof UserCreateInputSchema>> = {
+        id: user.id,
+        email: user.emailAddresses.find(
+          (email) => email.id === user.primaryEmailAddressId
+        )?.emailAddress,
+        firstName: user.firstName || undefined,
+        lastName: user.lastName || undefined,
+      };
 
-  const organizationMembership = await prisma.organizationMembership.create({
-    data: {
-      // Hard coding from Clerk (for now)
-      id: "orgmem_2dop73NRK5Z6faWgRiEk0IkUFQb",
-      role: "org:admin",
-      user: {
-        connect: {
+      const validatedUser = UserCreateInputSchema.safeParse(input);
+
+      if (!validatedUser.success) {
+        console.error(validatedUser.error.errors);
+        process.exit(1);
+      }
+
+      return prisma.user.upsert({
+        where: {
           id: user.id,
         },
-      },
-      organization: {
-        connect: {
+        update: {
+          ...validatedUser.data,
+        },
+        create: {
+          ...validatedUser.data,
+        },
+      });
+    }),
+  ]);
+}
+
+async function createOrganizationsFromClerk() {
+  const organizations = await clerkClient.organizations.getOrganizationList();
+
+  return prisma.$transaction([
+    ...organizations.data.map((organization) => {
+      const validatedBrokerage = OrganizationCreateInputSchema.safeParse({
+        id: organization.id,
+        name: organization.name,
+        slug: organization.slug,
+        imageUrl: organization.imageUrl,
+      });
+
+      if (!validatedBrokerage.success) {
+        console.error(validatedBrokerage.error.errors);
+        process.exit(1);
+      }
+
+      return prisma.organization.upsert({
+        where: {
           id: organization.id,
         },
-      },
-    },
-  });
+        update: {
+          ...validatedBrokerage.data,
+        },
+        create: {
+          ...validatedBrokerage.data,
+        },
+      });
+    }),
+  ]);
+}
+
+async function createOrganizationMembershipsFromClerk(
+  clerkOrganizationId: string
+) {
+  const memberships =
+    await clerkClient.organizations.getOrganizationMembershipList({
+      organizationId: clerkOrganizationId,
+    });
+
+  return prisma.$transaction([
+    ...memberships.data.map((membership) => {
+      if (!membership.publicUserData) {
+        console.error(
+          `Organization membership ${membership.id} does not have public user data`
+        );
+        process.exit(1);
+      }
+
+      const input: z.infer<typeof OrganizationMembershipCreateInputSchema> = {
+        id: membership.id,
+        role: membership.role,
+        user: {
+          connect: {
+            id: membership.publicUserData.userId,
+          },
+        },
+        organization: {
+          connect: {
+            id: membership.organization.id,
+          },
+        },
+      };
+
+      const validatedMembership =
+        OrganizationMembershipCreateInputSchema.safeParse(input);
+
+      if (!validatedMembership.success) {
+        console.error(validatedMembership.error.errors);
+        process.exit(1);
+      }
+
+      return prisma.organizationMembership.upsert({
+        where: {
+          id: membership.id,
+        },
+        update: {
+          ...validatedMembership.data,
+        },
+        create: {
+          ...validatedMembership.data,
+        },
+      });
+    }),
+  ]);
+}
+
+async function main() {
+  /**
+   * Create users
+   */
+  await createUsersFromClerk();
+
+  /**
+   * Create organizations
+   */
+  const organizations = await createOrganizationsFromClerk();
+
+  /**
+   * Create memberships
+   */
+  await Promise.all(
+    organizations.map((org) => createOrganizationMembershipsFromClerk(org.id))
+  );
 }
 
 main()
