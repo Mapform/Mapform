9ea1978414aa06df60ed8f5e0c3109bb6fe98b23
Nicholas Haley <hello@nichaley.com>
2024-06-03 21:12:52 -0400

    wip

--- apps/app/src/app/(authenticated)/(directories)/orgs/[orgSlug]/workspaces/[workspaceSlug]/page.tsx
+++ apps/app/src/app/(authenticated)/(directories)/orgs/[orgSlug]/workspaces/[workspaceSlug]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
+import { format } from "date-fns";
 import { prisma } from "@mapform/db";
-import { ChevronRightIcon } from "lucide-react";
 import WorkspaceLayout from "./workspace-layout";
 
 export default async function Organization({
@@ -20,6 +20,11 @@ export default async function Organization({
         where: {
           isPublished: false,
         },
+        include: {
+          _count: {
+            select: { formSubmission: true },
+          },
+        },
       },
     },
   });
@@ -35,62 +40,30 @@ export default async function Organization({
       workspaceId={workspace.id}
       workspaceSlug={params.workspaceSlug}
     >
-      <ul>
+      <ul className="flex flex-wrap gap-4">
         {workspace.forms.map((form) => (
-          // <li key={form.id}>
-          //   <Link href={`/forms/${form.id}`}>{form.name}</Link>
-          // </li>
-          <li
-            className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6"
-            key={form.id}
-          >
-            <div className="flex min-w-0 gap-x-4">
-              {/* <img
-                className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                src={person.imageUrl}
-                alt=""
-              /> */}
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">
-                  <Link href={`/forms/${form.id}`}>
-                    <span className="absolute inset-x-0 -top-px bottom-0" />
-                    {form.name}
-                  </Link>
-                </p>
-                <p className="mt-1 flex text-xs leading-5 text-gray-500">
-                  {/* <a
-                    href={`mailto:${person.email}`}
-                    className="relative truncate hover:underline"
-                  >
-                    {person.email}
-                  </a> */}
-                </p>
+          <li className="overflow-hidden rounded-xl border w-72" key={form.id}>
+            <Link href={`/forms/${form.id}`}>
+              <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-white p-6">
+                {form.name}
               </div>
-            </div>
-            <div className="flex shrink-0 items-center gap-x-4">
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                {/* <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-                {person.lastSeen ? (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    Last seen{" "}
-                    <time dateTime={person.lastSeenDateTime}>
-                      {person.lastSeen}
+              <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Form Submissions</dt>
+                  <dd className="text-gray-700">
+                    {form._count.formSubmission}
+                  </dd>
+                </div>
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Created</dt>
+                  <dd className="text-gray-700">
+                    <time dateTime={workspace.createdAt.toDateString()}>
+                      {format(form.createdAt, "MMMM do, yyyy")}
                     </time>
-                  </p>
-                ) : (
-                  <div className="mt-1 flex items-center gap-x-1.5">
-                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                      <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                    </div>
-                    <p className="text-xs leading-5 text-gray-500">Online</p>
-                  </div>
-                )} */}
-              </div>
-              <ChevronRightIcon
-                aria-hidden="true"
-                className="h-5 w-5 flex-none text-gray-400"
-              />
-            </div>
+                  </dd>
+                </div>
+              </dl>
+            </Link>
           </li>
         ))}
       </ul>
