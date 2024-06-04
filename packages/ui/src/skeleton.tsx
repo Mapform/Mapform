82fa1fbede8b251db20f6aad7e4a8c78f5eb8a9e
Nicholas Haley <hello@nichaley.com>
2024-06-03 21:29:13 -0400

    Create Skeleton component and loading ui

---
+++ packages/ui/src/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@mapform/lib/classnames";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
