39b88246e8e3cc04412e78590afd5740dfc5b4d8
Nicholas Haley <hello@nichaley.com>
2024-06-09 00:05:05 -0400

    wip - image block

--- packages/mapform/src/lib/custom-blocks/image.tsx
+++ packages/mapform/src/lib/custom-blocks/image.tsx
@@ -6,7 +6,7 @@ import {
 } from "@mapform/ui/components/popover";
 import { Input } from "@mapform/ui/components/input";
 import { Switch } from "@mapform/ui/components/switch";
-import { EllipsisIcon, ImageIcon } from "lucide-react";
+import { EllipsisIcon, ImageIcon, ImageOffIcon } from "lucide-react";
 import { Label } from "@mapform/ui/components/label";
 import { useStepContext } from "../../mapform/block-note/context";
 
@@ -14,18 +14,10 @@ export const Image = createReactBlockSpec(
   {
     type: "image",
     propSchema: {
-      // label: {
-      //   default: "My Label",
-      //   type: "string",
-      // },
-      // placeholder: {
-      //   default: "",
-      //   type: "string",
-      // },
-      // required: {
-      //   default: false,
-      //   type: "boolean",
-      // },
+      imageUrl: {
+        default: "",
+        type: "string",
+      },
     },
     content: "none",
   },
@@ -33,16 +25,34 @@ export const Image = createReactBlockSpec(
     render: ({ block, editor }) => {
       const { editable } = useStepContext();
 
+      if (editable) {
+        return (
+          <Popover>
+            <PopoverTrigger className="w-full">
+              <div className="w-full p-2 bg-gray-100 rounded text-gray-500 text-sm flex gap-2 items-center font-medium">
+                <ImageIcon />
+                <span>Add an image</span>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <Input type="file" />
+            </PopoverContent>
+          </Popover>
+        );
+      }
+
+      if (!block.props.imageUrl) {
+        return (
+          <div className="w-full p-2 bg-gray-100 rounded text-gray-500 text-sm flex justify-center">
+            <ImageOffIcon />
+          </div>
+        );
+      }
+
       return (
-        <Popover>
-          <PopoverTrigger className="w-full">
-            <div className="w-full p-2 bg-gray-100 rounded text-gray-500 text-sm flex gap-2 items-center font-medium">
-              <ImageIcon />
-              <span>Add an image</span>
-            </div>
-          </PopoverTrigger>
-          <PopoverContent>Upload</PopoverContent>
-        </Popover>
+        <div>
+          <img src={block.props.imageUrl} alt="Image" />
+        </div>
       );
     },
   }
