ac1c5e1ed2dbc58087b9e9adf62e13ab8ece3890
Nicholas Haley <hello@nichaley.com>
2024-06-19 15:34:29 -0400

    Fix overflow scroll issue on blocknote

--- packages/mapform/src/block-note/index.tsx
+++ packages/mapform/src/block-note/index.tsx
@@ -88,62 +88,64 @@ export function Blocknote({
   // Renders the editor instance using a React component.
   return (
     <div className="h-full flex flex-col">
-      {/* Content */}
-      <div className="overflow-y-auto p-4 pb-0">
-        {/* Title */}
-        {editable ? (
-          <AutoSizeTextArea
-            onChange={(val) => {
-              setUncontrolledTitle(val);
-              onTitleChange && onTitleChange(val);
-            }}
-            value={uncontrolledTitle}
-          />
-        ) : (
-          <h1 className="border-0 text-2xl font-bold w-full mb-2 p-0">
-            {title ?? "Untitled"}
-          </h1>
-        )}
+      <div className="flex-1 flex flex-col overflow-y-auto">
+        {/* Content */}
+        <div className="overflow-y-auto p-4 pb-0">
+          {/* Title */}
+          {editable ? (
+            <AutoSizeTextArea
+              onChange={(val) => {
+                setUncontrolledTitle(val);
+                onTitleChange && onTitleChange(val);
+              }}
+              value={uncontrolledTitle}
+            />
+          ) : (
+            <h1 className="border-0 text-2xl font-bold w-full mb-2 p-0">
+              {title ?? "Untitled"}
+            </h1>
+          )}
 
-        {/* Description */}
-        <BlockNoteView
+          {/* Description */}
+          <BlockNoteView
+            className="flex-1"
+            editable={editable}
+            editor={editor}
+            onChange={() => {
+              onDescriptionChange &&
+                onDescriptionChange({
+                  content: editor.document,
+                });
+            }}
+            sideMenu={false}
+            slashMenu={false}
+          >
+            <SuggestionMenuController
+              // eslint-disable-next-line @typescript-eslint/require-await -- Needs to return a Promise
+              getItems={async (query) => {
+                return filterSuggestionItems(
+                  [
+                    ...getDefaultReactSlashMenuItems(editor),
+
+                    insertPin(editor),
+                    insertTextInput(editor),
+                    insertImage(editor),
+                  ],
+                  query
+                );
+              }}
+              triggerCharacter="/"
+            />
+          </BlockNoteView>
+        </div>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions -- Disable a11y checks here */}
+        <div
           className="flex-1"
-          editable={editable}
-          editor={editor}
-          onChange={() => {
-            onDescriptionChange &&
-              onDescriptionChange({
-                content: editor.document,
-              });
+          onClick={() => {
+            editor.focus();
           }}
-          sideMenu={false}
-          slashMenu={false}
-        >
-          <SuggestionMenuController
-            // eslint-disable-next-line @typescript-eslint/require-await -- Needs to return a Promise
-            getItems={async (query) => {
-              return filterSuggestionItems(
-                [
-                  ...getDefaultReactSlashMenuItems(editor),
-
-                  insertPin(editor),
-                  insertTextInput(editor),
-                  insertImage(editor),
-                ],
-                query
-              );
-            }}
-            triggerCharacter="/"
-          />
-        </BlockNoteView>
+        />
       </div>
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions -- Disable a11y checks here */}
-      <div
-        className="flex-1"
-        onClick={() => {
-          editor.focus();
-        }}
-      />
       <div className="mt-auto flex justify-between p-4">
         <div className="gap-2">
           <Button
