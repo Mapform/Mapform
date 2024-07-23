6095c142060c755f674349d7bee00c549cf4a7e8
Nicholas Haley <hello@nichaley.com>
2024-07-23 08:54:20 -0400

    wip - create col whenever blocks updated

--- apps/app/src/server/actions/steps/update/index.ts
+++ apps/app/src/server/actions/steps/update/index.ts
@@ -19,26 +19,64 @@ export const updateStep = authAction
           organizationId: orgId,
         },
       },
+      include: {
+        dataset: {
+          include: {
+            columns: true,
+          },
+        },
+      },
     });
 
     if (!userForm) {
       throw new Error("User does not have access to this organization.");
     }
 
-    await prisma.step.update({
-      where: {
-        id: stepId,
-      },
-      // @ts-expect-error -- This is a valid update
-      data,
-    });
-    await prisma.form.update({
-      where: {
-        id: data.formId,
-      },
-      data: {
-        isDirty: true,
-      },
+    const inputBlocksToCreate = data.description?.content
+      .flatMap((block) => {
+        if (block.type === "pin" || block.type === "textInput") {
+          return block;
+        }
+
+        return undefined;
+      })
+      .filter((block) => {
+        return (
+          block !== undefined &&
+          !userForm.dataset?.columns.find((col) => col.name === block.id)
+        );
+      });
+
+    console.log(11111, inputBlocksToCreate);
+
+    await prisma.$transaction(async (tx) => {
+      await tx.step.update({
+        where: {
+          id: stepId,
+        },
+        // @ts-expect-error -- This is a valid update
+        data,
+      });
+
+      await tx.form.update({
+        where: {
+          id: data.formId,
+        },
+        data: {
+          isDirty: true,
+        },
+      });
+
+      await tx.column.createMany({
+        data: inputBlocksToCreate.map((block) => {
+          return {
+            name: block.id,
+            // TODO: Make this dynamic
+            dataType: "POINT",
+            datasetId: userForm.dataset?.id,
+          };
+        }),
+      });
     });
 
     revalidatePath("/");
--- apps/widget/src/server/actions/submit-form-step/index.ts
+++ apps/widget/src/server/actions/submit-form-step/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { v4 as uuidv4 } from "uuid";
 import { prisma } from "@mapform/db";
 import { revalidatePath } from "next/cache";
 import { getFormSchemaFromBlockNote } from "@mapform/blocknote";
@@ -35,84 +36,95 @@ export const submitFormStep = action
       throw new Error("Invalid payload");
     }
 
-    await Promise.all(
-      Object.entries(data).map(async ([key, value]) => {
-        const block = documentContent.find((b) => b.id === key);
+    console.log(1111, data);
 
-        if (block?.type === "textInput") {
-          return prisma.inputResponse.upsert({
-            create: {
-              formSubmissionId,
-              blockNoteId: key,
-              value: value as string,
-              stepId,
-            },
-            update: {
-              formSubmissionId,
-              blockNoteId: key,
-              value: value as string,
-            },
-            where: {
-              blockNoteId_formSubmissionId: {
-                blockNoteId: key,
-                formSubmissionId,
-              },
-            },
-          });
-        }
+    await prisma.$transaction(async (tx) => {
+      await Promise.all(
+        Object.entries(data).map(async ([key, value]) => {
+          const block = documentContent.find((b) => b.id === key);
 
-        if (block?.type === "pin") {
-          // First, create a Location with raw SQL query since Prisma doesn't support Postgis
-          await prisma.$transaction(async (tx) => {
-            const currentResponse = await tx.locationResponse.findUnique({
-              where: {
-                blockNoteId_formSubmissionId: {
-                  blockNoteId: key,
-                  formSubmissionId,
-                },
-              },
-            });
-            const currentLocationId = currentResponse?.locationId;
+          if (block?.type === "pin" && false) {
+            tx.$executeRaw`
+              INSERT INTO "PointCell" (id, cellvalueid, value)
+              VALUES(${uuidv4()}, ${cell.id}, ST_SetSRID(ST_MakePoint(${value.longitude}, ${value.latitude}), 4326))
+            `;
+          }
 
-            const locationId: { id: number }[] = await tx.$queryRaw`
-                INSERT INTO "Location" (geom)
-                VALUES(ST_SetSRID(ST_MakePoint(${value.longitude}, ${value.latitude}), 4326))
-                RETURNING id
-              `;
+          // if (block?.type === "textInput") {
+          //   return prisma.inputResponse.upsert({
+          //     create: {
+          //       formSubmissionId,
+          //       blockNoteId: key,
+          //       value: value as string,
+          //       stepId,
+          //     },
+          //     update: {
+          //       formSubmissionId,
+          //       blockNoteId: key,
+          //       value: value as string,
+          //     },
+          //     where: {
+          //       blockNoteId_formSubmissionId: {
+          //         blockNoteId: key,
+          //         formSubmissionId,
+          //       },
+          //     },
+          //   });
+          // }
 
-            if (!locationId[0]?.id) {
-              throw new Error("Failed to create location");
-            }
+          // if (block?.type === "pin") {
+          //   // First, create a Location with raw SQL query since Prisma doesn't support Postgis
+          //   await prisma.$transaction(async (tx) => {
+          //     const currentResponse = await tx.locationResponse.findUnique({
+          //       where: {
+          //         blockNoteId_formSubmissionId: {
+          //           blockNoteId: key,
+          //           formSubmissionId,
+          //         },
+          //       },
+          //     });
+          //     const currentLocationId = currentResponse?.locationId;
 
-            await tx.locationResponse.upsert({
-              create: {
-                formSubmissionId,
-                blockNoteId: key,
-                locationId: locationId[0].id,
-                stepId,
-              },
-              update: {
-                formSubmissionId,
-                blockNoteId: key,
-                locationId: locationId[0].id,
-              },
-              where: {
-                blockNoteId_formSubmissionId: {
-                  blockNoteId: key,
-                  formSubmissionId,
-                },
-              },
-            });
+          //     const locationId: { id: number }[] = await tx.$queryRaw`
+          //         INSERT INTO "Location" (geom)
+          //         VALUES(ST_SetSRID(ST_MakePoint(${value.longitude}, ${value.latitude}), 4326))
+          //         RETURNING id
+          //       `;
 
-            if (currentLocationId) {
-              await tx.$queryRaw`
-                DELETE FROM "Location" WHERE id = ${currentLocationId}
-              `;
-            }
-          });
-        }
-      })
-    );
+          //     if (!locationId[0]?.id) {
+          //       throw new Error("Failed to create location");
+          //     }
+
+          //     await tx.locationResponse.upsert({
+          //       create: {
+          //         formSubmissionId,
+          //         blockNoteId: key,
+          //         locationId: locationId[0].id,
+          //         stepId,
+          //       },
+          //       update: {
+          //         formSubmissionId,
+          //         blockNoteId: key,
+          //         locationId: locationId[0].id,
+          //       },
+          //       where: {
+          //         blockNoteId_formSubmissionId: {
+          //           blockNoteId: key,
+          //           formSubmissionId,
+          //         },
+          //       },
+          //     });
+
+          //     if (currentLocationId) {
+          //       await tx.$queryRaw`
+          //         DELETE FROM "Location" WHERE id = ${currentLocationId}
+          //       `;
+          //     }
+          //   });
+          // }
+        })
+      );
+    });
 
     revalidatePath(`/${step.formId}`);
   });
