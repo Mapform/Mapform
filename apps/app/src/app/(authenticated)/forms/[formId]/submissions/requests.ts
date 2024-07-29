00bfe2804960b5bcf663d9d48fe12d3dacaecfd5
Nicholas Haley <hello@nichaley.com>
2024-07-28 23:27:31 -0400

    Combine point data into submission results

--- apps/app/src/app/(authenticated)/forms/[formId]/submissions/requests.ts
+++ apps/app/src/app/(authenticated)/forms/[formId]/submissions/requests.ts
@@ -26,5 +26,48 @@ export async function getFormData({ formId }: { formId: string }) {
     },
   });
 
-  return form;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- It is necessary
+  const pointCellsWithLocation = (await prisma.$queryRaw`
+    SELECT "PointCell".id, ST_X("PointCell".value) AS longitude, ST_Y("PointCell".value) AS latitude
+    FROM "PointCell"
+    JOIN "CellValue" ON "PointCell"."cellvalueid" = "CellValue".id
+    JOIN "Row" ON "CellValue"."rowId" = "Row".id
+    JOIN "Dataset" ON "Row"."datasetId" = "Dataset".id
+    JOIN "Form" ON "Dataset".id = "Form"."datasetId"
+    WHERE "Form".id = ${formId};
+  `) as { id: string; longitude: string; latitude: string }[];
+
+  // Combine the pointCell data with the form data
+  const combined = form?.dataset?.rows.map((row) => {
+    const formSubmission = row.formSubmission;
+    const cellValues = row.cellValues.map((cellValue) => {
+      const pointCell = pointCellsWithLocation.find(
+        (pc) => pc.id === cellValue.pointCell?.id
+      );
+
+      return {
+        ...cellValue,
+        pointCell: pointCell
+          ? {
+              ...cellValue.pointCell,
+              value: `${pointCell.longitude},${pointCell.latitude}`,
+            }
+          : cellValue.pointCell,
+      };
+    });
+
+    return {
+      ...row,
+      formSubmission,
+      cellValues,
+    };
+  });
+
+  return {
+    ...form,
+    dataset: {
+      ...form?.dataset,
+      rows: combined,
+    },
+  };
 }
