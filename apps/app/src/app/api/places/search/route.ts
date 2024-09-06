133224ab89b3ef5616b24236382c19122ac97109
Nicholas Haley <hello@nichaley.com>
2024-09-06 16:31:38 -0400

    wip - create endpoint to query foursquare places

---
+++ apps/app/src/app/api/places/search/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: process.env.FOURSQUARE_API_KEY!,
+  },
+};
+
+export async function GET(request: Request) {
+  const query = new URL(request.url).searchParams.get("query") ?? "";
+
+  try {
+    const searchParams = new URLSearchParams({
+      query,
+    }).toString();
+
+    const response = await fetch(
+      `https://api.foursquare.com/v3/places/search?${searchParams}`,
+      options
+    );
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return NextResponse.json({ data });
+  } catch (e: unknown) {
+    return NextResponse.json({ msg: "Failure" });
+  }
+}
