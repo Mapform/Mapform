f9683c0674b4b1fa168b34db5301b982a6cae536
Nicholas Haley <hello@nichaley.com>
2024-09-16 13:10:41 -0400

    Rename create dataset to create dataset from csv

--- apps/app/src/data/datalayer/quick-create/index.ts
+++ apps/app/src/data/datalayer/quick-create/index.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 import { prisma } from "@mapform/db";
 import { authAction } from "~/lib/safe-action";
 import { createLayer } from "~/data/layers/create";
-import { createDataset } from "~/data/datasets/create";
+import { createDatasetFromCSV } from "~/data/datasets/create-from-csv";
 import { quickCreateDataLayerSchema } from "./schema";
 
 export const quickCreateDataLayer = authAction
@@ -20,7 +20,7 @@ export const quickCreateDataLayer = authAction
       throw new Error("Form not found");
     }
 
-    const datasetResponse = await createDataset({
+    const datasetResponse = await createDatasetFromCSV({
       name,
       workspaceId: form.workspaceId,
       data,
