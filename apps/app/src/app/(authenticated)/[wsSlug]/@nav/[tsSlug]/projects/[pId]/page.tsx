08ae1874f97dfd9e4cfb9f5e7775496c049a5b4e
Nicholas Haley <hello@nichaley.com>
2024-10-04 08:08:48 -0400

    Add back project actions

--- apps/app/src/app/(authenticated)/[wsSlug]/@nav/[tsSlug]/projects/[pId]/page.tsx
+++ apps/app/src/app/(authenticated)/[wsSlug]/@nav/[tsSlug]/projects/[pId]/page.tsx
@@ -1,12 +1,47 @@
 import { NavSlot } from "~/components/nav-slot";
+import { Button } from "@mapform/ui/components/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@mapform/ui/components/popover";
+import { SendIcon } from "lucide-react";
+import { getProjectWithTeamspace } from "~/data/projects/get-project-with-teamspace";
+import { ShareContent } from "./share-content";
 
-export default function Nav({
+export default async function Nav({
   params,
 }: {
   params: { wsSlug: string; tsSlug: string; pId: string };
 }) {
+  const projectResponse = await getProjectWithTeamspace({
+    id: params.pId,
+  });
+  const project = projectResponse?.data;
+
+  if (!project) {
+    return null;
+  }
+
   return (
     <NavSlot
+      actions={
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="sm" variant="ghost">
+              <SendIcon className="h-4 w-4 mr-2" />
+              Share
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent collisionPadding={16}>
+            <ShareContent
+              isDirty={project.isDirty}
+              numberOfVersions={project.childProjects.length}
+              projectId={params.pId}
+            />
+          </PopoverContent>
+        </Popover>
+      }
       tabs={[
         {
           name: "Create",
