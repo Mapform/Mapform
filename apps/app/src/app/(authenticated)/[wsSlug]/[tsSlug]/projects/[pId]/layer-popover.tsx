3d6ba0d1f481e6bf4995ee8ba5658550532a5279
Nicholas Haley <hello@nichaley.com>
2024-10-17 08:54:59 +0900

    wip - creating layer creator popover

--- apps/app/src/app/(authenticated)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover.tsx
+++ apps/app/src/app/(authenticated)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover.tsx
@@ -5,6 +5,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@mapform/ui/components/popover";
+import { usePage } from "./page-context";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useForm,
+  zodResolver,
+} from "@mapform/ui/components/form";
+import {
+  CreateLayerSchema,
+  createLayerSchema,
+} from "~/data/layers/create-layer/schema";
+import { toast } from "@mapform/ui/components/toaster";
+import { useAction } from "next-safe-action/hooks";
+import { createLayer } from "~/data/layers/create-layer";
+import { Input } from "@mapform/ui/components/input";
 
 interface LayerPopoverProps {
   // The trigger
@@ -12,10 +31,67 @@ interface LayerPopoverProps {
 }
 
 export function LayerPopover({ children }: LayerPopoverProps) {
+  const { availableDatasets, ...rest } = usePage();
+  const optimisticPage = rest.optimisticPage!;
+
+  const form = useForm<CreateLayerSchema>({
+    defaultValues: {
+      pageId: optimisticPage.id,
+    },
+    resolver: zodResolver(createLayerSchema),
+  });
+  const { execute, status } = useAction(createLayer, {
+    onSuccess: () => {
+      toast("Layer created successfully.");
+    },
+    onError: ({ error }) => {
+      if (error.serverError) {
+        toast(error.serverError);
+        return;
+      }
+
+      toast("There was an error creating the layer.");
+    },
+  });
+
+  const onSubmit = (values: CreateLayerSchema) => {
+    execute(values);
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
-      <PopoverContent>Layer editor</PopoverContent>
+      <PopoverContent>
+        <Form {...form}>
+          <form
+            className="flex flex-col flex-1"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="space-y-2">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={field.disabled}
+                        name={field.name}
+                        onChange={field.onChange}
+                        placeholder="New Layer"
+                        ref={field.ref}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </PopoverContent>
     </Popover>
   );
 }
