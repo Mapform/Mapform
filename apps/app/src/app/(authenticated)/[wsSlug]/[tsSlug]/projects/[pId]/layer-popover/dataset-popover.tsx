02d4f831976edfba657a11f5d2274a27af3ebf57
Nicholas Haley <hello@nichaley.com>
2024-10-19 17:25:49 +0900

    Adding ability to create empty dataset

--- apps/app/src/app/(authenticated)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover/dataset-popover.tsx
+++ apps/app/src/app/(authenticated)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover/dataset-popover.tsx
@@ -8,16 +8,23 @@ import {
   CommandGroup,
   CommandItem,
 } from "@mapform/ui/components/command";
-import { FormLabel, type UseFormReturn } from "@mapform/ui/components/form";
+import {
+  FormField,
+  FormLabel,
+  type UseFormReturn,
+} from "@mapform/ui/components/form";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@mapform/ui/components/popover";
 import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
 import { useState } from "react";
 import { useStandardLayout } from "~/app/(authenticated)/[wsSlug]/standard-layout/context";
+import { createEmptyDataset } from "~/data/datasets/create-empty-dataset";
 import type { CreateLayerSchema } from "~/data/layers/create-layer/schema";
+import { useProject } from "../project-context";
 
 interface DatasetPopoverProps {
   form: UseFormReturn<CreateLayerSchema>;
@@ -26,56 +33,63 @@ interface DatasetPopoverProps {
 export function DatasetPopover({ form }: DatasetPopoverProps) {
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState("");
-  const [value, setValue] = useState("");
+  const { optimisticProjectWithPages } = useProject();
   const { workspaceDirectory } = useStandardLayout();
+  const { executeAsync, status } = useAction(createEmptyDataset, {
+    onSuccess: () => {
+      form.setValue("datasetId", "");
+    },
+  });
 
   const availableDatasets = workspaceDirectory.teamspaces.flatMap(
     (ts) => ts.datasets,
   );
 
   return (
-    <Popover modal onOpenChange={setOpen} open={open}>
-      <FormLabel htmlFor="datasetSelect">Dataset</FormLabel>
-      <div className="flex w-full flex-shrink-0 justify-end">
-        <PopoverTrigger asChild>
-          <Button
-            className="ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-7 w-full items-center justify-between whitespace-nowrap rounded-md border-0 bg-stone-100 px-2 py-0.5 text-sm font-normal shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
-            id="datasetSelect"
-            size="icon-xs"
-            variant="ghost"
-          >
-            {value
-              ? availableDatasets.find((dataset) => dataset.id === value)?.name
-              : "Select dataset..."}
-            <ChevronsUpDownIcon className="size-4 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent align="start" className="w-[200px] p-0" side="right">
-          <Command>
-            <CommandInput
-              onValueChange={(v: string) => {
-                setQuery(v);
-              }}
-              placeholder="Search datasets..."
-              value={query}
-            />
-            <CommandList>
-              <CommandEmpty
-                className="data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground relative m-1 flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50"
-                onClick={() => {
-                  setQuery("");
-                  setOpen(false);
-                }}
+    <FormField
+      control={form.control}
+      name="datasetId"
+      render={({ field }) => (
+        <Popover modal onOpenChange={setOpen} open={open}>
+          <FormLabel htmlFor="datasetSelect">Dataset</FormLabel>
+          <div className="flex w-full flex-shrink-0 justify-end">
+            <PopoverTrigger asChild>
+              <Button
+                className="ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-7 w-full items-center justify-between whitespace-nowrap rounded-md border-0 bg-stone-100 px-2 py-0.5 text-sm font-normal shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
+                id="datasetSelect"
+                size="icon-xs"
+                variant="ghost"
               >
-                <p>Create: </p>
-                <p className="text-primary ml-1 block max-w-48 truncate font-semibold">
-                  {query}
-                </p>
-              </CommandEmpty>
-              <CommandGroup>
-                {query.length ? (
-                  <CommandItem
-                    onClick={() => {
+                {form.watch("datasetId")
+                  ? availableDatasets.find(
+                      (dataset) => dataset.id === field.value,
+                    )?.name
+                  : "Select dataset..."}
+                <ChevronsUpDownIcon className="size-4 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              align="start"
+              className="w-[200px] p-0"
+              side="right"
+            >
+              <Command>
+                <CommandInput
+                  onValueChange={(v: string) => {
+                    setQuery(v);
+                  }}
+                  placeholder="Search datasets..."
+                  value={query}
+                />
+                <CommandList>
+                  <CommandEmpty
+                    className="data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground relative m-1 flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50"
+                    onClick={async () => {
+                      console.log(11111);
+                      await executeAsync({
+                        name: query,
+                        teamspaceId: optimisticProjectWithPages.teamspaceId,
+                      });
                       setQuery("");
                       setOpen(false);
                     }}
@@ -84,32 +98,59 @@ export function DatasetPopover({ form }: DatasetPopoverProps) {
                     <p className="text-primary ml-1 block max-w-48 truncate font-semibold">
                       {query}
                     </p>
-                  </CommandItem>
-                ) : null}
-                {availableDatasets.map((dataset) => (
-                  <CommandItem
-                    key={dataset.id}
-                    keywords={[dataset.name]}
-                    onSelect={(currentValue) => {
-                      setValue(currentValue === value ? "" : currentValue);
-                      setOpen(false);
-                    }}
-                    value={dataset.id}
-                  >
-                    {dataset.name}
-                    <CheckIcon
-                      className={cn(
-                        "ml-auto size-4",
-                        value === dataset.id ? "opacity-100" : "opacity-0",
-                      )}
-                    />
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </div>
-    </Popover>
+                  </CommandEmpty>
+                  <CommandGroup>
+                    {query.length ? (
+                      <CommandItem
+                        disabled={status === "executing"}
+                        onClick={async () => {
+                          console.log(22222);
+                          await executeAsync({
+                            name: query,
+                            teamspaceId: optimisticProjectWithPages.teamspaceId,
+                          });
+                          console.log(33333);
+                          setQuery("");
+                          setOpen(false);
+                        }}
+                      >
+                        <p>Create: </p>
+                        <p className="text-primary ml-1 block max-w-48 truncate font-semibold">
+                          {query}
+                        </p>
+                      </CommandItem>
+                    ) : null}
+                    {availableDatasets.map((dataset) => (
+                      <CommandItem
+                        key={dataset.id}
+                        keywords={[dataset.name]}
+                        onSelect={(currentValue) => {
+                          form.setValue(
+                            "datasetId",
+                            currentValue === field.value ? "" : currentValue,
+                          );
+                          setOpen(false);
+                        }}
+                        value={dataset.id}
+                      >
+                        {dataset.name}
+                        <CheckIcon
+                          className={cn(
+                            "ml-auto size-4",
+                            field.value === dataset.id
+                              ? "opacity-100"
+                              : "opacity-0",
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </div>
+        </Popover>
+      )}
+    />
   );
 }
---
+++ apps/app/src/data/datasets/create-empty-dataset/index.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { db } from "@mapform/db";
+import { revalidatePath } from "next/cache";
+import { datasets } from "@mapform/db/schema";
+import { authAction } from "~/lib/safe-action";
+import { createEmptyDatasetSchema } from "./schema";
+
+export const createEmptyDataset = authAction
+  .schema(createEmptyDatasetSchema)
+  .action(async ({ parsedInput: { name, teamspaceId } }) => {
+    await db.insert(datasets).values({
+      name,
+      teamspaceId,
+    });
+
+    revalidatePath("/[wsSlug]/[tsSlug]/projects/[pId]/project", "page");
+  });
