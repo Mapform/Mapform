7af567b6f4c509c28cc8c84749dc636e98a6062d
Nicholas Haley <hello@nichaley.com>
2024-11-07 16:45:56 -0500

    Add a console log to debug

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,6 +2,7 @@ import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
 
+console.log(1111, process.env.DATABASE_URL);
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, {
   schema,
