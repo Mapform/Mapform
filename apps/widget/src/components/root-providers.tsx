131e1fe4bbeaa104ea9bcf96866db593b0c56b9e
Nicholas Haley <hello@nichaley.com>
2024-11-07 00:01:22 -0500

    Add db:deploy command

--- apps/widget/src/components/root-providers.tsx
+++ apps/widget/src/components/root-providers.tsx
@@ -4,11 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
-export default function RootProviders({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export function RootProviders({ children }: { children: React.ReactNode }) {
   const [client] = useState(
     new QueryClient({
       defaultOptions: {
