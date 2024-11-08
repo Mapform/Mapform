26093667a388e6704a0bd95fdc9f869f8a419277
Nicholas Haley <hello@nichaley.com>
2024-11-08 11:44:53 -0500

    Remove ws from db connection

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,12 +1,8 @@
-import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
+import { Pool } from "@neondatabase/serverless";
 import * as schema from "./schema";
 import { env } from "./env.mjs";
 
-neonConfig.webSocketConstructor = ws;
-
 const pool = new Pool({ connectionString: env.DATABASE_URL });
-export const db = drizzle(pool, {
-  schema,
-});
+
+export const db = drizzle(pool, { schema });
