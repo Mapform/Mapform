3b2073ac168e1d27cef97129ddf1c1fe584f7dae
Nicholas Haley <hello@nichaley.com>
2024-11-08 13:36:24 -0500

    Revert middleware changes

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -1,13 +1,13 @@
-// import { db } from "@mapform/db";
-// import { users, workspaceMemberships, workspaces } from "@mapform/db/schema";
-// import { eq } from "@mapform/db/utils";
+import { db } from "@mapform/db";
+import { users, workspaceMemberships, workspaces } from "@mapform/db/schema";
+import { eq } from "@mapform/db/utils";
 import { NextResponse } from "next/server";
 import { auth, BASE_PATH } from "~/lib/auth";
 
 const publicAppPaths = ["/onboarding", "/static"];
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- This shows an ugly TS error otherwise
-export default auth((req) => {
+export default auth(async (req) => {
   const reqUrl = new URL(req.url);
 
   const pathname = req.nextUrl.pathname;
@@ -31,34 +31,39 @@ export default auth((req) => {
    * Prevent requests to workspace that the user is not a member of
    */
   if (req.auth.user?.id && !isPublicAppPath) {
-    // const workspaceSlug = req.nextUrl.pathname.split("/")[1];
-    // const hasWorkspaceSlug =
-    //   Boolean(workspaceSlug) && workspaceSlug?.trim() !== "";
-    // const allowedWorkspaces = await db
-    //   .select()
-    //   .from(workspaceMemberships)
-    //   .innerJoin(users, eq(users.id, workspaceMemberships.userId))
-    //   .innerJoin(
-    //     workspaces,
-    //     eq(workspaces.id, workspaceMemberships.workspaceId),
-    //   )
-    //   .where(eq(users.id, req.auth.user.id));
-    // if (hasWorkspaceSlug) {
-    //   const hasAccessToWorkspace = allowedWorkspaces.find(
-    //     ({ workspace }) => workspace.slug === workspaceSlug,
-    //   );
-    //   if (!hasAccessToWorkspace) {
-    //     return NextResponse.redirect(new URL("/", req.url));
-    //   }
-    // } else if (allowedWorkspaces.length > 0) {
-    //   const firstWorkspace = allowedWorkspaces[0]?.workspace;
-    //   if (firstWorkspace) {
-    //     return NextResponse.redirect(
-    //       new URL(`/${firstWorkspace.slug}`, req.url),
-    //     );
-    //   }
-    //   return NextResponse.redirect(new URL(`/account`, req.url));
-    // }
+    const workspaceSlug = req.nextUrl.pathname.split("/")[1];
+    const hasWorkspaceSlug =
+      Boolean(workspaceSlug) && workspaceSlug?.trim() !== "";
+
+    const allowedWorkspaces = await db
+      .select()
+      .from(workspaceMemberships)
+      .innerJoin(users, eq(users.id, workspaceMemberships.userId))
+      .innerJoin(
+        workspaces,
+        eq(workspaces.id, workspaceMemberships.workspaceId),
+      )
+      .where(eq(users.id, req.auth.user.id));
+
+    if (hasWorkspaceSlug) {
+      const hasAccessToWorkspace = allowedWorkspaces.find(
+        ({ workspace }) => workspace.slug === workspaceSlug,
+      );
+
+      if (!hasAccessToWorkspace) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    } else if (allowedWorkspaces.length > 0) {
+      const firstWorkspace = allowedWorkspaces[0]?.workspace;
+
+      if (firstWorkspace) {
+        return NextResponse.redirect(
+          new URL(`/${firstWorkspace.slug}`, req.url),
+        );
+      }
+
+      return NextResponse.redirect(new URL(`/account`, req.url));
+    }
   }
 
   /**
