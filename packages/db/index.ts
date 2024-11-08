9a1517739c0282d9e63776dbc1164ed36c8ff647
Nicholas Haley <hello@nichaley.com>
2024-11-07 22:22:19 -0500

    Install ws for edge runtime

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,7 +1,10 @@
-import { Pool } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 import * as schema from "./schema";
 import { env } from "./env.mjs";
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 export const db = drizzle(pool, {
