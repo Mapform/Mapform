e1c5456784752865c226a7ed16b0f72d440aa323
Nicholas Haley <hello@nichaley.com>
2024-11-08 13:03:15 -0500

    add console logs to support debugging

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,6 +3,7 @@ import { Pool } from "@neondatabase/serverless";
 import * as schema from "./schema";
 import { env } from "./env.mjs";
 
+console.log(11111, env.DATABASE_URL);
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 export const db = drizzle(pool, { schema });
