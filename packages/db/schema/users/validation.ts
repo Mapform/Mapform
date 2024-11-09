e2c80faafe3c4b2799d2bf1fbbc18a239df70bbc
Nicholas Haley <nicholashaley@Nicholass-MacBook-Pro.local>
2024-11-09 11:28:06 -0500

    Create sessions auth service

--- packages/db/schema/users/validation.ts
+++ packages/db/schema/users/validation.ts
@@ -1,6 +1,6 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import type { z } from "zod";
-import { users } from "./schema";
+import { users, sessions } from "./schema";
 
 export const insertUserSchema = createInsertSchema(users, {
   name: (schema) => schema.name.min(3),
@@ -10,3 +10,4 @@ export const selectUserSchema = createSelectSchema(users);
 
 export type InsertUser = z.infer<typeof insertUserSchema>;
 export type User = typeof users.$inferSelect;
+export type Session = typeof sessions.$inferSelect;
