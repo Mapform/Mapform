3f44abc7950a3b282d23d9dac1e89ce9eeffae96
Nicholas Haley <hello@nichaley.com>
2024-11-10 22:46:57 -0500

    change filename

--- apps/app/env.js
+++ apps/app/env.js
@@ -7,7 +7,6 @@ export const env = createEnv({
     DATABASE_URL: z.string().url().min(1),
     VERCEL_ENV: z.string().optional(),
     GEOAPIFY_API_KEY: z.string().min(1),
-    AUTH_SECRET: z.string().min(1),
     VERCEL_URL: z.string().min(1).optional(),
     NODE_ENV: z.enum(["development", "production"]),
   },
@@ -19,7 +18,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     VERCEL_ENV: process.env.VERCEL_ENV,
     GEOAPIFY_API_KEY: process.env.GEOAPIFY_API_KEY,
-    AUTH_SECRET: process.env.AUTH_SECRET,
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
     VERCEL_URL: process.env.VERCEL_URL,
