6d87b73c2bf9556db3f6f9b98f8ff942db16e8e0
Nicholas Haley <hello@nichaley.com>
2024-11-10 23:31:18 -0500

    Fix login link

--- apps/app/src/app/api/auth/magic-link/route.ts
+++ apps/app/src/app/api/auth/magic-link/route.ts
@@ -1,6 +1,11 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { validateMagicLink } from "@mapform/backend/auth/validate-magic-link";
 import { validateMagicLinkSchema } from "@mapform/backend/auth/validate-magic-link/schema";
+import { env } from "#env.mjs";
+
+const baseUrl = env.VERCEL_URL
+  ? `https://${env.VERCEL_URL}`
+  : "http://localhost:3000";
 
 /**
  * TODO: Rate limit this (e.g. 1 request per minute)
@@ -13,8 +18,8 @@ export async function GET(request: NextRequest) {
   try {
     await validateMagicLink({ token });
 
-    return NextResponse.redirect("http://localhost:3000/");
+    return NextResponse.redirect(`${baseUrl}/`);
   } catch (e) {
-    return NextResponse.redirect("http://localhost:3000/signin?error=1");
+    return NextResponse.redirect(`${baseUrl}/signin?error=1`);
   }
 }
