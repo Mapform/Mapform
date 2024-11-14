1d0c560ba0d496e38df6ce5f9bb252b4605269d2
Nicholas Haley <hello@nichaley.com>
2024-11-14 10:32:07 -0500

    Fix base url

--- packages/backend/env.mjs
+++ packages/backend/env.mjs
@@ -6,10 +6,10 @@ export const env = createEnv({
     VERCEL_URL: z.string().min(1).optional(),
   },
   client: {
-    NEXT_PUBLIC_BASE_URL: z.string().min(1),
+    NEXT_PUBLIC_BASE_URL: z.string().url().min(1),
   },
   runtimeEnv: {
     VERCEL_URL: process.env.VERCEL_URL,
-    NEXT_PUBLIC_BASE_URL: process.NODE_ENV === "development" ? "http://localhost:3000" : process.env.VERCEL_ENV === "preview" ? process.env.VERCEL_URL : "https://alpha.mapform.co",
+    NEXT_PUBLIC_BASE_URL: process.NODE_ENV === "development" ? "http://localhost:3000" : process.env.VERCEL_ENV === "preview" ? `https://${process.env.VERCEL_URL}` : "https://alpha.mapform.co",
   },
 });
