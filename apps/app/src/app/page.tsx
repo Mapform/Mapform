526b3c6e8a567421b2da0a0ab8125cdb435a8673
Nicholas Haley <hello@nichaley.com>
2024-11-24 00:51:12 -0500

    style update

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Page() {
         <section className="lg:flex lg:items-center lg:gap-x-12">
           <div className="mx-auto flex max-w-2xl flex-col items-center text-center sm:items-baseline sm:text-left lg:mx-0 lg:flex-auto">
             <span className="flex-grow-0">
-              <Badge>Alpha</Badge>
+              <Badge variant="outline">Alpha</Badge>
             </span>
-            <h1 className="text-foreground mt-4 text-6xl">
+            <h1 className="text-muted-foreground mt-4 text-6xl font-medium">
               A Place For
               <br />
               Everything.
               <br />
-              <div className="mt-4">
+              <div className="text-foreground mt-4">
                 Everything
                 <br />
                 In Its Place.
