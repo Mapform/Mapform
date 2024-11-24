25a48a4eba72361dfab076695665eb39bd2149f2
Nicholas Haley <hello@nichaley.com>
2024-11-24 09:23:39 -0500

    Hide markers

--- apps/app/src/components/landing/globe.tsx
+++ apps/app/src/components/landing/globe.tsx
@@ -38,9 +38,25 @@ export function Globe() {
       opacity: 0.8,
       scale: 1.2,
       baseColor: [1, 1, 1],
-      markerColor: [255, 255, 255],
+      markerColor: [0 / 255, 0 / 255, 0 / 255],
       glowColor: [1.2, 1.2, 1.2],
-      markers: [],
+      markers: [
+        // { location: [37.7749, -122.4194], size: 0.05 }, // San Francisco, USA
+        // { location: [40.7128, -74.006], size: 0.05 }, // New York City, USA
+        // { location: [48.8566, 2.3522], size: 0.05 }, // Paris, France
+        // { location: [35.6895, 139.6917], size: 0.05 }, // Tokyo, Japan
+        // { location: [51.5074, -0.1278], size: 0.05 }, // London, UK
+        // { location: [-33.8688, 151.2093], size: 0.05 }, // Sydney, Australia
+        // { location: [55.7558, 37.6173], size: 0.05 }, // Moscow, Russia
+        // { location: [-23.5505, -46.6333], size: 0.05 }, // São Paulo, Brazil
+        // { location: [1.3521, 103.8198], size: 0.05 }, // Singapore
+        // { location: [19.4326, -99.1332], size: 0.05 }, // Mexico City, Mexico
+        // { location: [52.52, 13.405], size: 0.05 }, // Berlin, Germany
+        // { location: [34.0522, -118.2437], size: 0.05 }, // Los Angeles, USA
+        // { location: [28.6139, 77.209], size: 0.05 }, // Delhi, India
+        // { location: [31.2304, 121.4737], size: 0.05 }, // Shanghai, China
+        // { location: [6.5244, 3.3792], size: 0.05 }, // Lagos, Nigeria
+      ],
       onRender: (state) => {
         // This prevents rotation while dragging
         if (!pointerInteracting.current) {
