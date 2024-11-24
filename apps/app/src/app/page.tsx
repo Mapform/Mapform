30521295c1c828812ca97acff37b0e6089707d89
Nicholas Haley <hello@nichaley.com>
2024-11-24 09:53:21 -0500

    Add better mobile style for site

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -6,13 +6,15 @@ import { Nav } from "~/components/landing/nav";
 
 export default function Page() {
   return (
-    <div className="flex h-screen flex-col bg-white">
+    <div className="relative flex h-screen flex-col">
       <Nav />
       <div className="mx-auto max-w-7xl px-6 pt-10 sm:pt-20 lg:px-8">
         <section className="lg:flex lg:items-center lg:gap-x-12">
           <div className="mx-auto flex max-w-2xl flex-col items-center text-center sm:items-baseline sm:text-left lg:mx-0 lg:flex-auto">
             <span className="flex-grow-0">
-              <Badge variant="outline">Now in Alpha</Badge>
+              <Badge className="bg-white" variant="outline">
+                Now in Alpha
+              </Badge>
             </span>
             <h1 className="text-muted-foreground mt-4 text-4xl font-medium lg:text-6xl">
               A Place For
@@ -44,8 +46,8 @@ export default function Page() {
               </Link>
             </div>
           </div>
-          <div className="hidden sm:mt-24 lg:mt-0 lg:block lg:flex-shrink-0 lg:flex-grow">
-            <div className="w-[550px]">
+          <div className="absolute inset-0 -z-10 opacity-15 max-lg:h-screen max-lg:w-screen lg:relative lg:z-0 lg:mt-0 lg:size-[550px] lg:flex-shrink-0 lg:flex-grow lg:opacity-100">
+            <div className="flex h-full w-full justify-center overflow-hidden">
               <Globe />
             </div>
           </div>
