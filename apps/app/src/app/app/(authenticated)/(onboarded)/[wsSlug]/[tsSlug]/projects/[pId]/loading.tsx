b18e7ac0f64f7c2a6edd0f32d016b5c2fcf312fc
Nicholas Haley <hello@nichaley.com>
2024-12-05 23:04:26 -0500

    Add loader skeleton to project

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/loading.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/loading.tsx
@@ -1,3 +1,15 @@
+import { Skeleton } from "@mapform/ui/components/skeleton";
+
 export default function Loading() {
-  return "Loading...";
+  return (
+    <div className="p-4">
+      <div className="w-[360px] space-y-3 pl-8">
+        <Skeleton className="h-[100px] w-full rounded-lg" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-2/3" />
+        </div>
+      </div>
+    </div>
+  );
 }
