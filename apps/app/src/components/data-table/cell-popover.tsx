50b5a1a22dceaf46e1839286d93fefab5e67ab94
Nicholas Haley <hello@nichaley.com>
2024-12-16 23:16:42 -0500

    Remove memo from CellPopover

--- apps/app/src/components/data-table/cell-popover.tsx
+++ apps/app/src/components/data-table/cell-popover.tsx
@@ -21,7 +21,7 @@ import { TableCell } from "@mapform/ui/components/table";
 import { DateTimePicker } from "@mapform/ui/components/datetime-picker";
 import { flexRender, type Cell } from "@tanstack/react-table";
 import { useAction } from "next-safe-action/hooks";
-import { memo, useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   schema,
   BlocknoteEditor,
@@ -39,247 +39,235 @@ import { upsertCellAction } from "~/data/cells/upsert-cell";
 
 const accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
-export const CellPopover = memo(
-  ({
-    cell,
-    dataset,
-  }: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- We don't need the full cell definition
-    cell: Cell<any, any>;
-    dataset: NonNullable<GetDataset["data"]>;
-  }) => {
-    const cellEl = useRef<HTMLTableCellElement>(null);
-    const type = dataset.columns.find(
-      (column) => column.id === cell.column.id,
-    )?.type;
+export function CellPopover({
+  cell,
+  dataset,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- We don't need the full cell definition
+  cell: Cell<any, any>;
+  dataset: NonNullable<GetDataset["data"]>;
+}) {
+  const cellEl = useRef<HTMLTableCellElement>(null);
+  const type = dataset.columns.find(
+    (column) => column.id === cell.column.id,
+  )?.type;
 
-    const form = useForm<UpsertCellSchema>({
-      defaultValues: {
-        rowId: cell.row.id,
-        columnId: cell.column.id,
-        value:
-          type === "number" ? cell.getValue()?.toString() : cell.getValue(),
-        type,
-      },
-      resolver: zodResolver(upsertCellSchema),
-    });
-    const { execute: executeUpsertCell } = useAction(upsertCellAction);
-    const [open, setOpen] = useState(false);
+  const form = useForm<UpsertCellSchema>({
+    defaultValues: {
+      rowId: cell.row.id,
+      columnId: cell.column.id,
+      value: type === "number" ? cell.getValue()?.toString() : cell.getValue(),
+      type,
+    },
+    resolver: zodResolver(upsertCellSchema),
+  });
+  const { execute: executeUpsertCell } = useAction(upsertCellAction);
+  const [open, setOpen] = useState(false);
 
-    const onSubmit = (values: UpsertCellSchema) => {
-      setOpen(false);
-      executeUpsertCell(values);
-    };
+  const onSubmit = (values: UpsertCellSchema) => {
+    setOpen(false);
+    executeUpsertCell(values);
+  };
 
-    const renderCellContent = useCallback(() => {
-      const v = form.getValues();
-      const { type: parsedType, value } = upsertCellSchema.parse(v);
+  const renderCellContent = useCallback(() => {
+    const v = form.getValues();
+    const { type: parsedType, value } = upsertCellSchema.parse(v);
 
-      if (parsedType === "point") {
-        if (!value) {
-          return null;
-        }
-
-        return (
-          <span className="font-mono">
-            {value.x.toFixed(4)},{value.y.toFixed(4)}
-          </span>
-        );
+    if (parsedType === "point") {
+      if (!value) {
+        return null;
       }
 
-      if (parsedType === "richtext") {
-        if (!value) {
-          return null;
-        }
-
-        return (
-          <Badge className="cursor-pointer" variant="secondary">
-            Click to view
-          </Badge>
-        );
-      }
-
-      if (parsedType === "date") {
-        if (!value) {
-          return null;
-        }
-
-        return format(new Date(value), "PP hh:mm b", { locale: enUS });
-      }
-
-      return value;
-    }, [form]);
-
-    // Used for radio buttons
-    if (!type) {
       return (
-        <TableCell>
-          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-        </TableCell>
+        <span className="font-mono">
+          {value.x.toFixed(4)},{value.y.toFixed(4)}
+        </span>
       );
     }
 
-    if (type === "bool") {
+    if (parsedType === "richtext") {
+      if (!value) {
+        return null;
+      }
+
       return (
-        <TableCell>
-          <Form {...form}>
-            <form>
-              <FormField
-                control={form.control}
-                name="value"
-                render={({ field }) => (
-                  <FormItem className="flex-1">
-                    <FormControl>
-                      <Switch
-                        checked={Boolean(field.value)}
-                        name={field.name}
-                        onCheckedChange={(e) => {
-                          field.onChange(e);
-                          const formVal = form.getValues();
-                          executeUpsertCell(formVal);
-                        }}
-                        size="sm"
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-        </TableCell>
+        <Badge className="cursor-pointer" variant="secondary">
+          Click to view
+        </Badge>
       );
     }
 
-    const renderField = () => {
-      if (type === "string") {
-        return (
-          <StringInput
-            form={
-              form as UseFormReturn<
-                Extract<UpsertCellSchema, { type: "string" }>
-              >
-            }
-          />
-        );
+    if (parsedType === "date") {
+      if (!value) {
+        return null;
       }
 
-      if (type === "number") {
-        return (
-          <NumberInput
-            form={
-              form as UseFormReturn<
-                Extract<UpsertCellSchema, { type: "number" }>
-              >
-            }
-          />
-        );
-      }
+      return format(new Date(value), "PP hh:mm b", { locale: enUS });
+    }
 
-      if (type === "date") {
-        return (
-          <DateInput
-            form={
-              form as UseFormReturn<Extract<UpsertCellSchema, { type: "date" }>>
-            }
-          />
-        );
-      }
+    return value;
+  }, [form]);
 
-      if (type === "point") {
-        return (
-          <PointInput
-            form={
-              form as UseFormReturn<
-                Extract<UpsertCellSchema, { type: "point" }>
-              >
-            }
-          />
-        );
-      }
-
-      if (type === "icon") {
-        return (
-          <EmojiInput
-            form={
-              form as UseFormReturn<Extract<UpsertCellSchema, { type: "icon" }>>
-            }
-          />
-        );
-      }
-
-      return (
-        <RichtextInput
-          form={
-            form as UseFormReturn<
-              Extract<UpsertCellSchema, { type: "richtext" }>
-            >
-          }
-        />
-      );
-    };
-
+  // Used for radio buttons
+  if (!type) {
     return (
-      <TableCell
-        onClick={() => {
-          setOpen(true);
-        }}
-        onKeyDown={(e) => {
-          if (!open && e.key === "Enter") {
-            setOpen(true);
-          }
-        }}
-        ref={cellEl}
-        tabIndex={0}
-      >
+      <TableCell>
+        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+      </TableCell>
+    );
+  }
+
+  if (type === "bool") {
+    return (
+      <TableCell>
         <Form {...form}>
-          <form
-            onKeyDown={(e) => {
-              if (type !== "richtext" && e.key === "Enter") {
-                onSubmit(form.getValues());
-                cellEl.current?.focus();
-              }
-
-              if (e.key === "Escape") {
-                setOpen(false);
-                cellEl.current?.focus();
-              }
-
-              if (type === "point" && e.key === "Backspace") {
-                form.setValue("value", null);
-                onSubmit(form.getValues());
-                cellEl.current?.focus();
-              }
-            }}
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <Popover
-              onOpenChange={(val) => {
-                setOpen(val);
-                const formVal = form.getValues();
-
-                if (!val) {
-                  onSubmit(formVal);
-                }
-              }}
-              open={open}
-            >
-              {renderCellContent()}
-              <PopoverAnchor />
-              <PopoverContent
-                align="start"
-                className="w-full min-w-72 overflow-hidden p-0"
-                side="top"
-              >
-                {renderField()}
-              </PopoverContent>
-            </Popover>
+          <form>
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormControl>
+                    <Switch
+                      checked={Boolean(field.value)}
+                      name={field.name}
+                      onCheckedChange={(e) => {
+                        field.onChange(e);
+                        const formVal = form.getValues();
+                        executeUpsertCell(formVal);
+                      }}
+                      size="sm"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
           </form>
         </Form>
       </TableCell>
     );
-  },
-);
-CellPopover.displayName = "CellPopover";
+  }
+
+  const renderField = () => {
+    if (type === "string") {
+      return (
+        <StringInput
+          form={
+            form as UseFormReturn<Extract<UpsertCellSchema, { type: "string" }>>
+          }
+        />
+      );
+    }
+
+    if (type === "number") {
+      return (
+        <NumberInput
+          form={
+            form as UseFormReturn<Extract<UpsertCellSchema, { type: "number" }>>
+          }
+        />
+      );
+    }
+
+    if (type === "date") {
+      return (
+        <DateInput
+          form={
+            form as UseFormReturn<Extract<UpsertCellSchema, { type: "date" }>>
+          }
+        />
+      );
+    }
+
+    if (type === "point") {
+      return (
+        <PointInput
+          form={
+            form as UseFormReturn<Extract<UpsertCellSchema, { type: "point" }>>
+          }
+        />
+      );
+    }
+
+    if (type === "icon") {
+      return (
+        <EmojiInput
+          form={
+            form as UseFormReturn<Extract<UpsertCellSchema, { type: "icon" }>>
+          }
+        />
+      );
+    }
+
+    return (
+      <RichtextInput
+        form={
+          form as UseFormReturn<Extract<UpsertCellSchema, { type: "richtext" }>>
+        }
+      />
+    );
+  };
+
+  return (
+    <TableCell
+      onClick={() => {
+        setOpen(true);
+      }}
+      onKeyDown={(e) => {
+        if (!open && e.key === "Enter") {
+          setOpen(true);
+        }
+      }}
+      ref={cellEl}
+      tabIndex={0}
+    >
+      <Form {...form}>
+        <form
+          onKeyDown={(e) => {
+            if (type !== "richtext" && e.key === "Enter") {
+              onSubmit(form.getValues());
+              cellEl.current?.focus();
+            }
+
+            if (e.key === "Escape") {
+              setOpen(false);
+              cellEl.current?.focus();
+            }
+
+            if (type === "point" && e.key === "Backspace") {
+              form.setValue("value", null);
+              onSubmit(form.getValues());
+              cellEl.current?.focus();
+            }
+          }}
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <Popover
+            onOpenChange={(val) => {
+              setOpen(val);
+              const formVal = form.getValues();
+
+              if (!val) {
+                onSubmit(formVal);
+              }
+            }}
+            open={open}
+          >
+            {renderCellContent()}
+            <PopoverAnchor />
+            <PopoverContent
+              align="start"
+              className="w-full min-w-72 overflow-hidden p-0"
+              side="top"
+            >
+              {renderField()}
+            </PopoverContent>
+          </Popover>
+        </form>
+      </Form>
+    </TableCell>
+  );
+}
 
 function StringInput({
   form,
