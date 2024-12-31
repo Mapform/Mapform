97e7e335171641c2d92b21c2dea1ff5b8516ea41
Nicholas Haley <hello@nichaley.com>
2024-12-31 12:13:59 -0500

    Fix data migration file

--- packages/db/data-migrations/0024_blue_snowbird.ts
+++ packages/db/data-migrations/0024_blue_snowbird.ts
@@ -1,7 +1,14 @@
 import { Stripe } from "stripe";
-import { db } from "..";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { env } from "../env.mjs";
-import { plans } from "../schema";
+import * as schema from "../schema";
+
+neonConfig.webSocketConstructor = ws;
+
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
@@ -32,7 +39,7 @@ async function runDataMigration() {
       });
 
       // Create a plan for each workspace
-      await db.insert(plans).values({
+      await db.insert(schema.plans).values({
         name: "Basic",
         workspaceSlug: workspace.slug,
         stripeCustomerId: customer.id,
