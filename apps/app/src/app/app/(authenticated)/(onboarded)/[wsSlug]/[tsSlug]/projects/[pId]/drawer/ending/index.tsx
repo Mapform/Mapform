2b740dbccbdc2b83dddf1d338152660284ae163e
Nicholas Haley <hello@nichaley.com>
2025-01-11 15:06:32 -0800

    wip

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/drawer/ending/index.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/drawer/ending/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   SidebarContent,
   SidebarGroup,
@@ -7,21 +9,47 @@ import {
   SidebarMenuItem,
   SidebarRightMenuButton,
 } from "@mapform/ui/components/sidebar";
-import { FlagIcon } from "lucide-react";
+import { ExternalLinkIcon, FlagIcon } from "lucide-react";
+import type { Ending } from "@mapform/db/schema";
+import type { ReactElement } from "react";
+import { useProject } from "../../project-context";
+
+const endingContent: Record<
+  Ending["endingType"],
+  {
+    title: string;
+    icon: ReactElement;
+  }
+> = {
+  page: {
+    title: "Page",
+    icon: <FlagIcon />,
+  },
+  redirect: {
+    title: "Redirect",
+    icon: <ExternalLinkIcon />,
+  },
+};
 
 export function Ending() {
+  const { projectWithPages } = useProject();
+
   return (
     <SidebarContent>
       <SidebarGroup>
         <SidebarGroupLabel>Ending</SidebarGroupLabel>
         <SidebarGroupContent>
           <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarRightMenuButton className="pr-8">
-                <FlagIcon />
-                <span className="truncate text-sm">Redirect</span>
-              </SidebarRightMenuButton>
-            </SidebarMenuItem>
+            {projectWithPages.ending ? (
+              <SidebarMenuItem>
+                <SidebarRightMenuButton className="pr-8">
+                  {endingContent[projectWithPages.ending.endingType].icon}
+                  <span className="truncate text-sm">
+                    {endingContent[projectWithPages.ending.endingType].title}
+                  </span>
+                </SidebarRightMenuButton>
+              </SidebarMenuItem>
+            ) : null}
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
