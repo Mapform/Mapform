8eb80e1690f2f399babf23ac4ab3fd604d763e36
Nicholas Haley <hello@nichaley.com>
2025-02-20 16:50:28 -0500

    Fix bug where inserting a text input would throw an error

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/project/index.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/project/index.tsx
@@ -15,6 +15,7 @@ import {
 import { CustomBlockProvider, type CustomBlock } from "@mapform/blocknote";
 import { Blocknote } from "~/components/mapform/block-note";
 import { LocationSearchDrawer } from "./location-search-drawer";
+import { Form as DummyForm, useForm } from "@mapform/ui/components/form";
 
 function Project() {
   const {
@@ -28,6 +29,10 @@ function Project() {
     updatePageOptimistic,
     updateSelectedFeatureOptimistic,
   } = useProject();
+
+  // This is just used to prevent the input blocks from throwing an error when
+  // calling useFormContext
+  const dummyForm = useForm();
 
   const [drawerValues, setDrawerValues] = useState<string[]>([
     "page-content",
@@ -68,278 +73,175 @@ function Project() {
   }
 
   return (
-    <div className="flex flex-1 justify-center overflow-hidden p-4">
-      <div className="flex flex-1">
-        <MapformContent
-          isEditing
-          drawerValues={drawerValues}
-          onDrawerValuesChange={setDrawerValues}
-          pageData={currentPageData}
-        >
-          <CustomBlockProvider
+    <DummyForm {...dummyForm}>
+      <div className="flex flex-1 justify-center overflow-hidden p-4">
+        <div className="flex flex-1">
+          <MapformContent
             isEditing
-            imageBlock={{
-              onImageUpload: async (file: File) => {
-                const compressedFile = await compressImage(
-                  file,
-                  0.8,
-                  2000,
-                  2000,
-                  1000,
-                );
-                const formData = new FormData();
-                formData.append("image", compressedFile);
-
-                const response = await uploadImageServer.executeAsync(formData);
-
-                if (response?.serverError) {
-                  return null;
-                }
-
-                return response?.data?.url || null;
-              },
-            }}
+            drawerValues={drawerValues}
+            onDrawerValuesChange={setDrawerValues}
+            pageData={currentPageData}
           >
-            <MapformDrawer
-              onClose={() => {
-                setDrawerValues(
-                  drawerValues.filter((v) => v !== "page-content"),
-                );
-              }}
-              value="page-content"
-            >
-              <Blocknote
-                description={currentPage.content as { content: CustomBlock[] }}
-                icon={currentPage.icon}
-                includeFormBlocks={
-                  projectWithPages.formsEnabled &&
-                  currentPage.pageType === "page"
-                }
-                key={currentPage.id}
-                onDescriptionChange={(val) => {
-                  debouncedUpdatePageDescription({
-                    id: currentPage.id,
-                    content: val,
-                  });
-                }}
-                onIconChange={(val) => {
-                  updatePageOptimistic({
-                    ...currentPage,
-                    icon: val,
-                  });
+            <CustomBlockProvider
+              isEditing
+              imageBlock={{
+                onImageUpload: async (file: File) => {
+                  const compressedFile = await compressImage(
+                    file,
+                    0.8,
+                    2000,
+                    2000,
+                    1000,
+                  );
+                  const formData = new FormData();
+                  formData.append("image", compressedFile);
 
-                  updatePageServer.execute({
-                    id: currentPage.id,
-                    icon: val,
-                  });
-                }}
-                onTitleChange={(val) => {
-                  debouncedUpdatePageTitle({
-                    id: currentPage.id,
-                    title: val,
-                  });
-                }}
-                title={currentPage.title}
-              />
-            </MapformDrawer>
-            <MapformDrawer
-              onClose={() => {
-                setDrawerValues(drawerValues.filter((v) => v !== "feature"));
-              }}
-              value="feature"
-            >
-              <Blocknote
-                description={
-                  selectedFeature?.description?.richtextCell?.value ?? undefined
-                }
-                icon={selectedFeature?.icon?.iconCell?.value}
-                key={`${currentPage.id}-${selectedFeature?.rowId}`}
-                onDescriptionChange={(value) => {
-                  if (!selectedFeature?.description) {
-                    return;
+                  const response =
+                    await uploadImageServer.executeAsync(formData);
+
+                  if (response?.serverError) {
+                    return null;
                   }
 
-                  debouncedUpdateCellRichtext({
-                    type: "richtext",
-                    value,
-                    rowId: selectedFeature.rowId,
-                    columnId: selectedFeature.description.columnId,
-                  });
-                }}
-                onIconChange={(value) => {
-                  if (!selectedFeature?.icon || !currentPageData) {
-                    return;
-                  }
-
-                  if (selectedFeature.icon.iconCell) {
-                    updateSelectedFeatureOptimistic({
-                      ...selectedFeature,
-                      icon: {
-                        ...selectedFeature.icon,
-                        iconCell: {
-                          ...selectedFeature.icon.iconCell,
-                          value,
-                        },
-                      },
-                    });
-                  }
-
-                  upsertCellServer.execute({
-                    type: "icon",
-                    value,
-                    rowId: selectedFeature.rowId,
-                    columnId: selectedFeature.icon.columnId,
-                  });
-                }}
-                onTitleChange={(value) => {
-                  if (!selectedFeature?.title) {
-                    return;
-                  }
-
-                  debouncedUpdateCellString({
-                    type: "string",
-                    value,
-                    rowId: selectedFeature.rowId,
-                    columnId: selectedFeature.title.columnId,
-                  });
-                }}
-                title={selectedFeature?.title?.stringCell?.value}
-              />
-            </MapformDrawer>
-            <LocationSearchDrawer currentPage={currentPage} />
-            {/* <MarkerEditDrawer currentPage={currentPage} /> */}
-          </CustomBlockProvider>
-          <MapformDrawerButton
-            onOpen={() => setDrawerValues([...drawerValues, "page-content"])}
-          />
-          <MapformMap
-            initialViewState={{
-              longitude: currentPage.center.x,
-              latitude: currentPage.center.y,
-              zoom: currentPage.zoom,
-              bearing: currentPage.bearing,
-              pitch: currentPage.pitch,
-              padding: {
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-              },
-            }}
-          >
-            <EditBar key={currentPage.id} />
-          </MapformMap>
-        </MapformContent>
-        {/* <MapForm
-          currentPage={currentPage}
-          isEditing
-          includeFormBlocks={
-            projectWithPages.formsEnabled && currentPage.pageType === "page"
-          }
-          mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-          onDescriptionChange={(content, type) => {
-            if (type === "page") {
-              debouncedUpdatePageDescription({ id: currentPage.id, content });
-              return;
-            }
-
-            if (!selectedFeature?.description) {
-              return;
-            }
-
-            debouncedUpdateCellRichtext({
-              type: "richtext",
-              value: content,
-              rowId: selectedFeature.rowId,
-              columnId: selectedFeature.description.columnId,
-            });
-          }}
-          onIconChange={(icon, type) => {
-            if (type === "page") {
-              updatePageOptimistic({
-                ...currentPage,
-                icon,
-              });
-
-              updatePageServer.execute({
-                id: currentPage.id,
-                icon,
-              });
-
-              return;
-            }
-
-            if (!selectedFeature?.icon || !currentPageData) {
-              return;
-            }
-
-            if (selectedFeature.icon.iconCell) {
-              updateSelectedFeatureOptimistic({
-                ...selectedFeature,
-                icon: {
-                  ...selectedFeature.icon,
-                  iconCell: {
-                    ...selectedFeature.icon.iconCell,
-                    value: icon,
-                  },
+                  return response?.data?.url || null;
                 },
-              });
-            }
+              }}
+            >
+              <MapformDrawer
+                onClose={() => {
+                  setDrawerValues(
+                    drawerValues.filter((v) => v !== "page-content"),
+                  );
+                }}
+                value="page-content"
+              >
+                <Blocknote
+                  description={
+                    currentPage.content as { content: CustomBlock[] }
+                  }
+                  icon={currentPage.icon}
+                  includeFormBlocks={
+                    projectWithPages.formsEnabled &&
+                    currentPage.pageType === "page"
+                  }
+                  key={currentPage.id}
+                  onDescriptionChange={(val) => {
+                    debouncedUpdatePageDescription({
+                      id: currentPage.id,
+                      content: val,
+                    });
+                  }}
+                  onIconChange={(val) => {
+                    updatePageOptimistic({
+                      ...currentPage,
+                      icon: val,
+                    });
 
-            upsertCellServer.execute({
-              type: "icon",
-              value: icon,
-              rowId: selectedFeature.rowId,
-              columnId: selectedFeature.icon.columnId,
-            });
-          }}
-          onImageUpload={async (file: File) => {
-            const compressedFile = await compressImage(
-              file,
-              0.8,
-              2000,
-              2000,
-              1000,
-            );
-            const formData = new FormData();
-            formData.append("image", compressedFile);
+                    updatePageServer.execute({
+                      id: currentPage.id,
+                      icon: val,
+                    });
+                  }}
+                  onTitleChange={(val) => {
+                    debouncedUpdatePageTitle({
+                      id: currentPage.id,
+                      title: val,
+                    });
+                  }}
+                  title={currentPage.title}
+                />
+              </MapformDrawer>
+              <MapformDrawer
+                onClose={() => {
+                  setDrawerValues(drawerValues.filter((v) => v !== "feature"));
+                }}
+                value="feature"
+              >
+                <Blocknote
+                  description={
+                    selectedFeature?.description?.richtextCell?.value ??
+                    undefined
+                  }
+                  icon={selectedFeature?.icon?.iconCell?.value}
+                  key={`${currentPage.id}-${selectedFeature?.rowId}`}
+                  onDescriptionChange={(value) => {
+                    if (!selectedFeature?.description) {
+                      return;
+                    }
 
-            const response = await uploadImageServer.executeAsync(formData);
+                    debouncedUpdateCellRichtext({
+                      type: "richtext",
+                      value,
+                      rowId: selectedFeature.rowId,
+                      columnId: selectedFeature.description.columnId,
+                    });
+                  }}
+                  onIconChange={(value) => {
+                    if (!selectedFeature?.icon || !currentPageData) {
+                      return;
+                    }
 
-            if (response?.serverError) {
-              return null;
-            }
+                    if (selectedFeature.icon.iconCell) {
+                      updateSelectedFeatureOptimistic({
+                        ...selectedFeature,
+                        icon: {
+                          ...selectedFeature.icon,
+                          iconCell: {
+                            ...selectedFeature.icon.iconCell,
+                            value,
+                          },
+                        },
+                      });
+                    }
 
-            return response?.data?.url || null;
-          }}
-          onTitleChange={(title, type) => {
-            if (type === "page") {
-              debouncedUpdatePageTitle({
-                id: currentPage.id,
-                title,
-              });
+                    upsertCellServer.execute({
+                      type: "icon",
+                      value,
+                      rowId: selectedFeature.rowId,
+                      columnId: selectedFeature.icon.columnId,
+                    });
+                  }}
+                  onTitleChange={(value) => {
+                    if (!selectedFeature?.title) {
+                      return;
+                    }
 
-              return;
-            }
-
-            if (!selectedFeature?.title) {
-              return;
-            }
-
-            debouncedUpdateCellString({
-              type: "string",
-              value: title,
-              rowId: selectedFeature.rowId,
-              columnId: selectedFeature.title.columnId,
-            });
-          }}
-          pageData={currentPageData}
-          selectedFeature={selectedFeature}
-        >
-          <EditBar key={currentPage.id} />
-        </MapForm> */}
+                    debouncedUpdateCellString({
+                      type: "string",
+                      value,
+                      rowId: selectedFeature.rowId,
+                      columnId: selectedFeature.title.columnId,
+                    });
+                  }}
+                  title={selectedFeature?.title?.stringCell?.value}
+                />
+              </MapformDrawer>
+              <LocationSearchDrawer currentPage={currentPage} />
+            </CustomBlockProvider>
+            <MapformDrawerButton
+              onOpen={() => setDrawerValues([...drawerValues, "page-content"])}
+            />
+            <MapformMap
+              initialViewState={{
+                longitude: currentPage.center.x,
+                latitude: currentPage.center.y,
+                zoom: currentPage.zoom,
+                bearing: currentPage.bearing,
+                pitch: currentPage.pitch,
+                padding: {
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                },
+              }}
+            >
+              <EditBar key={currentPage.id} />
+            </MapformMap>
+          </MapformContent>
+        </div>
       </div>
-    </div>
+    </DummyForm>
   );
 }
 
