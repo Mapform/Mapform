3b1da2bdb62a5155fea2578b75e3c4da56d3dd89
Nicholas Haley <hello@nichaley.com>
2025-02-20 21:34:37 -0500

    wip

--- apps/app/src/app/(marketing)/tabs.tsx
+++ apps/app/src/app/(marketing)/tabs.tsx
@@ -3,45 +3,73 @@
 import { motion } from "motion/react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 import Image from "next/image";
+import {
+  BookMarkedIcon,
+  EarthIcon,
+  SquareArrowOutUpRightIcon,
+  TableIcon,
+  TextCursorInputIcon,
+} from "lucide-react";
 
 const TABS = [
   {
-    title: "Map View",
-    image: "/static/images/map-view.png",
+    id: "map",
+    title: "Beautiful Maps",
+    image: "/static/images/map.jpeg",
     alt: "Map view interface",
+    icon: EarthIcon,
   },
   {
-    title: "Data View",
-    image: "/static/images/data-view.png",
-    alt: "Data view interface",
+    id: "dataset",
+    title: "Datasets",
+    image: "/static/images/dataset.png",
+    alt: "Dataset interface",
+    icon: TableIcon,
   },
   {
-    title: "Analysis",
-    image: "/static/images/analysis.png",
-    alt: "Analysis interface",
+    id: "guide",
+    title: "Guides",
+    image: "/static/images/guide.png",
+    alt: "Guide interface",
+    icon: BookMarkedIcon,
+  },
+  {
+    id: "forms",
+    title: "Forms",
+    image: "/static/images/forms.jpeg",
+    alt: "Forms interface",
+    icon: TextCursorInputIcon,
+  },
+  {
+    id: "publish",
+    title: "Publish",
+    image: "/static/images/share.png",
+    alt: "Publish interface",
+    icon: SquareArrowOutUpRightIcon,
   },
 ];
 
 export function TabsShowcase() {
   return (
-    <TabsPrimitive.Root
-      defaultValue="Map View"
-      className="mx-auto w-full max-w-3xl"
-    >
+    <TabsPrimitive.Root defaultValue="map" className="mx-auto w-full max-w-3xl">
       <TabsPrimitive.List className="flex gap-2">
         {TABS.map((tab) => (
-          <TabsPrimitive.Trigger key={tab.title} value={tab.title}>
+          <TabsPrimitive.Trigger
+            className="flex items-center gap-2 font-medium"
+            key={tab.id}
+            value={tab.id}
+          >
+            <tab.icon className="size-5" />
             {tab.title}
           </TabsPrimitive.Trigger>
         ))}
       </TabsPrimitive.List>
-
       {TABS.map((tab) => (
-        <TabsPrimitive.Content key={tab.title} value={tab.title}>
+        <TabsPrimitive.Content key={tab.id} value={tab.id}>
           <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 20 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             className="overflow-hidden rounded-lg border"
           >
             <Image
