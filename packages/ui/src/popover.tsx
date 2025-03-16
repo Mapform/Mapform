f693b40fb6290ced636b516dece95ae0623314fa
Nicholas Haley <hello@nichaley.com>
2025-03-16 11:42:44 -0400

    Fix issue with popover

--- packages/ui/src/popover.tsx
+++ packages/ui/src/popover.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
-import { FocusScope } from "@radix-ui/react-focus-scope";
+// import { FocusScope } from "@radix-ui/react-focus-scope";
 import { DismissableLayer } from "@radix-ui/react-dismissable-layer";
 import { cn } from "@mapform/lib/classnames";
 
@@ -17,20 +17,20 @@ const PopoverContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
-    <FocusScope asChild loop trapped>
-      <DismissableLayer>
-        <PopoverPrimitive.Content
-          align={align}
-          className={cn(
-            "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border px-3 py-2 text-sm shadow-md outline-none",
-            className,
-          )}
-          ref={ref}
-          sideOffset={sideOffset}
-          {...props}
-        />
-      </DismissableLayer>
-    </FocusScope>
+    {/* <FocusScope asChild loop trapped> */}
+    <DismissableLayer>
+      <PopoverPrimitive.Content
+        align={align}
+        className={cn(
+          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border px-3 py-2 text-sm shadow-md outline-none",
+          className,
+        )}
+        ref={ref}
+        sideOffset={sideOffset}
+        {...props}
+      />
+    </DismissableLayer>
+    {/* </FocusScope> */}
   </PopoverPrimitive.Portal>
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
