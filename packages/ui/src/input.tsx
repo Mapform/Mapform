ffe2e609bf96bdf83f343f0ab356a62601c14730
Nicholas Haley <hello@nichaley.com>
2025-03-16 23:35:53 -0400

    Fix input text size

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -3,7 +3,7 @@ import { cn } from "@mapform/lib/classnames";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const inputVariants = cva(
-  "flex w-full rounded-md border text-sm shadow-sm transition-colors file:border-0 file:bg-background file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+  "flex w-full rounded-md border text-base md:text-sm shadow-sm transition-colors file:border-0 file:bg-background file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
   {
     variants: {
       variant: {
