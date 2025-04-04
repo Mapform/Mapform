e0ecf3a77420d898e877e0fd32287d9dc54b5e35
Nicholas Haley <hello@nichaley.com>
2025-04-04 08:38:06 -0400

    Add ability to delete feature

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/project/feature-drawer/blocknote-controls/feature-settings-popover.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/project/feature-drawer/blocknote-controls/feature-settings-popover.tsx
@@ -7,9 +7,43 @@ import {
 } from "@mapform/ui/components/dropdown-menu";
 import { EllipsisIcon, Trash2Icon } from "lucide-react";
 import { Button } from "@mapform/ui/components/button";
+import { useAction } from "next-safe-action/hooks";
+import { deleteRowsAction } from "~/data/rows/delete-rows";
+import { useProject } from "../../../project-context";
+import { useSetQueryString } from "@mapform/lib/hooks/use-set-query-string";
+import { toast } from "@mapform/ui/components/toaster";
 
 export const FeatureSettingsPopover = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { selectedFeature } = useProject();
+  const setQueryString = useSetQueryString();
+
+  const { execute: executeDeleteRows, isPending } = useAction(
+    deleteRowsAction,
+    {
+      onError: ({ error }) => {
+        toast({
+          title: "Uh oh! Something went wrong.",
+          description:
+            error.serverError ?? "There was an error deleting the feature.",
+        });
+      },
+    },
+  );
+
+  const handleDelete = () => {
+    if (!selectedFeature) return;
+
+    executeDeleteRows({
+      rowIds: [selectedFeature.rowId],
+    });
+
+    // Clear the feature from the URL
+    setQueryString({
+      key: "feature",
+      value: null,
+    });
+  };
 
   return (
     <DropdownMenu onOpenChange={setIsOpen} open={isOpen}>
@@ -24,8 +58,9 @@ export const FeatureSettingsPopover = () => {
         className="w-[200px] overflow-hidden"
       >
         <DropdownMenuItem
-          // onSelect={onDelete}
+          onSelect={handleDelete}
           className="flex items-center gap-2"
+          disabled={isPending}
         >
           <Trash2Icon className="flex-shrink-0 size-4" />
           Delete feature
