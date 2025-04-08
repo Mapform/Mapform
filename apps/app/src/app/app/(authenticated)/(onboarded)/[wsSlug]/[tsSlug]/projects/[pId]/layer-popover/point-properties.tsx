4a4c7c9750f25164ce27e456ad3a1be1a2949f0b
Nicholas Haley <hello@nichaley.com>
2025-04-08 10:20:02 -0400

    stlying

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover/point-properties.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/[tsSlug]/projects/[pId]/layer-popover/point-properties.tsx
@@ -51,7 +51,7 @@ export function PointProperties({
 
   return (
     <>
-      <div className="col-span-2 mt-1 w-full border-t pt-3">
+      <div className="col-span-2 mt-2 w-full border-t pt-3">
         <h3 className="-mb-2 text-xs font-semibold leading-6 text-stone-400">
           Properties
         </h3>
@@ -88,7 +88,7 @@ export function PointProperties({
         name="pointProperties.iconColumnId"
         type="icon"
       />
-      <div className="col-span-2 mt-2 w-full border-t pt-3">
+      <div className="col-span-2 mt-1 w-full border-t pt-3">
         <h3 className="-mb-2 text-xs font-semibold leading-6 text-stone-400">
           Styles
         </h3>
