07639b6af53da3ff1d2ba7c63ad75fa6c40f0deb
Nicholas Haley <hello@nichaley.com>
2025-04-20 23:39:10 -0400

    Add routing endpoint

---
+++ apps/app/src/app/api/places/routing/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import type { GeoapifyPlace } from "@mapform/map-utils/types";
+import { env } from "~/env.mjs";
+import { getCurrentSession } from "~/data/auth/get-current-session";
+
+type Mode = "drive" | "bicycle" | "walk";
+
+export async function GET(request: Request) {
+  try {
+    const session = await getCurrentSession();
+
+    if (!session?.data?.user) {
+      return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
+    }
+
+    const mode: Mode =
+      (new URL(request.url).searchParams.get("mode") as Mode | undefined) ??
+      "drive";
+    const waypoints = new URL(request.url).searchParams.get("waypoints") ?? "";
+
+    const requestParams = new URLSearchParams({
+      apiKey: env.GEOAPIFY_API_KEY,
+      format: "json",
+      mode,
+      waypoints,
+    }).toString();
+
+    const response = await fetch(
+      `https://api.geoapify.com/v1/routing?${requestParams}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data: GeoapifyPlace = await response.json();
+
+    return NextResponse.json({ data });
+  } catch (e: unknown) {
+    return NextResponse.json({ msg: "Failure" });
+  }
+}
