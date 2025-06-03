c399ec8199ef4480e8e6ba8b3f8636b55700d690
Nicholas Haley <hello@nichaley.com>
2025-06-03 00:08:06 -0400

    Put back marker enum

--- packages/db/schema/layers/validation.ts
+++ packages/db/schema/layers/validation.ts
@@ -21,8 +21,14 @@ export type Layer = typeof layers.$inferSelect;
 /**
  * POINT LAYERS
  */
-export const insertPointLayerSchema = createInsertSchema(pointLayers);
-export const selectPointLayerSchema = createSelectSchema(pointLayers);
+export const insertPointLayerSchema = createInsertSchema(pointLayers, {
+  // Must be a hex code
+  color: z.string().length(7).regex(/^#/).optional(),
+});
+export const selectPointLayerSchema = createSelectSchema(pointLayers, {
+  // Must be a hex code
+  color: z.string().length(7).regex(/^#/).optional(),
+});
 
 export type InsertPointLayer = z.infer<typeof insertPointLayerSchema>;
 export type PointLayer = typeof pointLayers.$inferSelect;
