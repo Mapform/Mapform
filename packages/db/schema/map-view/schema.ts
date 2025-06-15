9fb574b9cbdb5c747fcd093e01b7d2acbef48858
Nicholas Haley <hello@nichaley.com>
2025-06-15 14:11:49 -0400

    Add basic view support

---
+++ packages/db/schema/map-view/schema.ts
@@ -0,0 +1,25 @@
+import { uuid } from "drizzle-orm/pg-core";
+
+import { timestamp } from "drizzle-orm/pg-core";
+
+import { pgTable } from "drizzle-orm/pg-core";
+import { projects } from "../projects/schema";
+
+/**
+ * PARENT CELL
+ */
+export const mapViews = pgTable("map_view", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  projectId: uuid("project_id")
+    .notNull()
+    .references(() => projects.id, { onDelete: "cascade" }),
+
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
--- packages/db/schema/projects/relations.ts
+++ packages/db/schema/projects/relations.ts
@@ -1,10 +1,14 @@
 import { relations } from "drizzle-orm";
 import { teamspaces } from "../teamspaces/schema";
 import { projects } from "./schema";
+import { mapViews } from "../map-view/schema";
+import { tableViews } from "../table-view/schema";
 
 export const projectsRelations = relations(projects, ({ one }) => ({
   teamspace: one(teamspaces, {
     fields: [projects.teamspaceId],
     references: [teamspaces.id],
   }),
+  tableView: one(tableViews),
+  mapView: one(mapViews),
 }));
