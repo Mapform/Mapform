a75d0be93762b286dda8e53ab1a468ddba03604d
Nicholas Haley <hello@nichaley.com>
2025-06-15 13:47:31 -0400

    Run migrations

--- packages/db/schema/index.ts
+++ packages/db/schema/index.ts
@@ -5,16 +5,10 @@ export * from "./workspace-memberships/schema";
 export * from "./teamspaces/schema";
 export * from "./teamspace-memberships/schema";
 export * from "./projects/schema";
-export * from "./pages/schema";
-export * from "./layers/schema";
-export * from "./layers-to-pages/schema";
-export * from "./datasets/schema";
 export * from "./rows/schema";
 export * from "./columns/schema";
-export * from "./cells/schema";
 export * from "./plans/schema";
 export * from "./blobs/schema";
-export * from "./form-submissions/schema";
 export * from "./folders/schema";
 
 // RELATIONS
@@ -24,16 +18,10 @@ export * from "./workspace-memberships/relations";
 export * from "./teamspaces/relations";
 export * from "./teamspace-memberships/relations";
 export * from "./projects/relations";
-export * from "./pages/relations";
-export * from "./layers/relations";
-export * from "./layers-to-pages/relations";
-export * from "./datasets/relations";
 export * from "./rows/relations";
 export * from "./columns/relations";
-export * from "./cells/relations";
 export * from "./plans/relations";
 export * from "./blobs/relations";
-export * from "./form-submissions/relations";
 export * from "./folders/relations";
 
 // VALIDATIONS
@@ -42,14 +30,8 @@ export * from "./workspaces/validation";
 export * from "./workspace-memberships/validation";
 export * from "./teamspaces/validation";
 export * from "./projects/validation";
-export * from "./pages/validation";
-export * from "./layers/validation";
-export * from "./layers-to-pages/validation";
-export * from "./datasets/validation";
 export * from "./rows/validation";
 export * from "./columns/validation";
-export * from "./cells/validation";
 export * from "./plans/validation";
 export * from "./blobs/validation";
-export * from "./form-submissions/validation";
 export * from "./folders/validation";
--- packages/db/schema/teamspaces/relations.ts
+++ packages/db/schema/teamspaces/relations.ts
@@ -2,12 +2,10 @@ import { relations } from "drizzle-orm";
 import { teamspaceMemberships } from "../teamspace-memberships/schema";
 import { projects } from "../projects/schema";
 import { workspaces } from "../workspaces/schema";
-import { datasets } from "../datasets/schema";
 import { teamspaces } from "./schema";
 
 export const teamspacesRelations = relations(teamspaces, ({ one, many }) => ({
   projects: many(projects),
-  datasets: many(datasets),
   teamspaceMemberships: many(teamspaceMemberships),
   workspace: one(workspaces, {
     fields: [teamspaces.workspaceSlug],
