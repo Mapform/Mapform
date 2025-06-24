ef4c7396890adb9cf7983aef319d89fe3b12c081
Nicholas Haley <hello@nichaley.com>
2025-06-24 16:25:03 -0400

    Create migrations

---
+++ packages/db/migrations/0001_smiling_shinko_yamashiro.sql
@@ -0,0 +1,212 @@
+CREATE TYPE "public"."workspace_role" AS ENUM('owner', 'member');--> statement-breakpoint
+CREATE TYPE "public"."teamspace_role" AS ENUM('owner', 'member');--> statement-breakpoint
+CREATE TYPE "public"."visibility" AS ENUM('public', 'closed');--> statement-breakpoint
+CREATE TYPE "public"."column_type" AS ENUM('string', 'bool', 'number', 'date');--> statement-breakpoint
+CREATE TYPE "public"."view_type" AS ENUM('table', 'map');--> statement-breakpoint
+CREATE TABLE "magic_link" (
+	"token" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"expires" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(256),
+	"email" text NOT NULL,
+	"image" text,
+	"hasOnboarded" boolean DEFAULT false NOT NULL,
+	"workspaceGuideCompleted" boolean DEFAULT false NOT NULL,
+	"projectGuideCompleted" boolean DEFAULT false NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "workspace" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"icon" varchar(256),
+	"slug" varchar(256) NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "workspace_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE "workspace_membership" (
+	"user_id" uuid NOT NULL,
+	"workspace_id" uuid NOT NULL,
+	"workspace_role" "workspace_role" NOT NULL,
+	CONSTRAINT "workspace_membership_user_id_workspace_id_pk" PRIMARY KEY("user_id","workspace_id")
+);
+--> statement-breakpoint
+CREATE TABLE "teamspace" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"slug" varchar(256) NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"workspace_slug" varchar NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "teamspace_workspace_slug_slug_unique" UNIQUE("workspace_slug","slug")
+);
+--> statement-breakpoint
+CREATE TABLE "teamspace_membership" (
+	"user_id" uuid NOT NULL,
+	"teamspace_id" uuid NOT NULL,
+	"teamspace_role" "teamspace_role" NOT NULL,
+	CONSTRAINT "teamspace_membership_user_id_teamspace_id_pk" PRIMARY KEY("user_id","teamspace_id")
+);
+--> statement-breakpoint
+CREATE TABLE "project" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(256),
+	"description" varchar(512),
+	"icon" varchar(256),
+	"position" smallint DEFAULT 0 NOT NULL,
+	"visibility" "visibility" DEFAULT 'closed' NOT NULL,
+	"folder_id" uuid,
+	"teamspace_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "row" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(256),
+	"description" jsonb,
+	"icon" varchar(256),
+	"geometry" geometry,
+	"row_embedding" vector(1536),
+	"project_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "column" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"type" "column_type" NOT NULL,
+	"project_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "plan" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(64) NOT NULL,
+	"stripe_customer_id" text NOT NULL,
+	"stripe_subscription_id" text,
+	"stripe_product_id" text,
+	"stripe_price_id" text,
+	"subscription_status" varchar(20),
+	"row_limit" integer NOT NULL,
+	"storage_limit" integer DEFAULT 10000000 NOT NULL,
+	"workspace_slug" varchar NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "plan_stripe_customer_id_unique" UNIQUE("stripe_customer_id"),
+	CONSTRAINT "plan_stripe_subscription_id_unique" UNIQUE("stripe_subscription_id"),
+	CONSTRAINT "plan_workspace_slug_unique" UNIQUE("workspace_slug")
+);
+--> statement-breakpoint
+CREATE TABLE "blob" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"workspace_id" uuid NOT NULL,
+	"url" varchar(2048) NOT NULL,
+	"size" integer NOT NULL,
+	"queued_for_deletion_date" timestamp with time zone,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "blob_url_unique" UNIQUE("url")
+);
+--> statement-breakpoint
+CREATE TABLE "folder" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"icon" varchar(256),
+	"parent_id" uuid,
+	"teamspace_id" uuid NOT NULL,
+	"position" smallint DEFAULT 0 NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "map_view" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"center" geometry(point) NOT NULL,
+	"view_id" uuid NOT NULL,
+	CONSTRAINT "map_view_unq" UNIQUE("view_id")
+);
+--> statement-breakpoint
+CREATE TABLE "table_view" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"view_id" uuid NOT NULL,
+	CONSTRAINT "table_view_unq" UNIQUE("view_id")
+);
+--> statement-breakpoint
+CREATE TABLE "view" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"project_id" uuid NOT NULL,
+	"type" "view_type" NOT NULL,
+	"name" text,
+	"position" smallint DEFAULT 0 NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "boolean_cell" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"value" boolean,
+	"cell_id" uuid NOT NULL,
+	CONSTRAINT "bool_cell_unq" UNIQUE("cell_id")
+);
+--> statement-breakpoint
+CREATE TABLE "cell" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"row_id" uuid NOT NULL,
+	"column_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "cell_row_id_column_id_unique" UNIQUE("row_id","column_id")
+);
+--> statement-breakpoint
+CREATE TABLE "date_cell" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"value" timestamp with time zone,
+	"cell_id" uuid NOT NULL,
+	CONSTRAINT "date_cell_unq" UNIQUE("cell_id")
+);
+--> statement-breakpoint
+CREATE TABLE "number_cell" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"value" numeric,
+	"cell_id" uuid NOT NULL,
+	CONSTRAINT "number_cell_unq" UNIQUE("cell_id")
+);
+--> statement-breakpoint
+CREATE TABLE "string_cell" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"value" text,
+	"cell_id" uuid NOT NULL,
+	CONSTRAINT "string_cell_unq" UNIQUE("cell_id")
+);
+--> statement-breakpoint
+ALTER TABLE "workspace_membership" ADD CONSTRAINT "workspace_membership_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "workspace_membership" ADD CONSTRAINT "workspace_membership_workspace_id_workspace_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "public"."workspace"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "teamspace" ADD CONSTRAINT "teamspace_workspace_slug_workspace_slug_fk" FOREIGN KEY ("workspace_slug") REFERENCES "public"."workspace"("slug") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE "teamspace_membership" ADD CONSTRAINT "teamspace_membership_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "teamspace_membership" ADD CONSTRAINT "teamspace_membership_teamspace_id_teamspace_id_fk" FOREIGN KEY ("teamspace_id") REFERENCES "public"."teamspace"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "project" ADD CONSTRAINT "project_folder_id_folder_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."folder"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "project" ADD CONSTRAINT "project_teamspace_id_teamspace_id_fk" FOREIGN KEY ("teamspace_id") REFERENCES "public"."teamspace"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "row" ADD CONSTRAINT "row_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "column" ADD CONSTRAINT "column_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "plan" ADD CONSTRAINT "plan_workspace_slug_workspace_slug_fk" FOREIGN KEY ("workspace_slug") REFERENCES "public"."workspace"("slug") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "folder" ADD CONSTRAINT "folder_teamspace_id_teamspace_id_fk" FOREIGN KEY ("teamspace_id") REFERENCES "public"."teamspace"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "folder" ADD CONSTRAINT "custom_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."folder"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "map_view" ADD CONSTRAINT "map_view_view_id_view_id_fk" FOREIGN KEY ("view_id") REFERENCES "public"."view"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "table_view" ADD CONSTRAINT "table_view_view_id_view_id_fk" FOREIGN KEY ("view_id") REFERENCES "public"."view"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "view" ADD CONSTRAINT "view_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "boolean_cell" ADD CONSTRAINT "boolean_cell_cell_id_cell_id_fk" FOREIGN KEY ("cell_id") REFERENCES "public"."cell"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "cell" ADD CONSTRAINT "cell_row_id_row_id_fk" FOREIGN KEY ("row_id") REFERENCES "public"."row"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "cell" ADD CONSTRAINT "cell_column_id_column_id_fk" FOREIGN KEY ("column_id") REFERENCES "public"."column"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "date_cell" ADD CONSTRAINT "date_cell_cell_id_cell_id_fk" FOREIGN KEY ("cell_id") REFERENCES "public"."cell"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "number_cell" ADD CONSTRAINT "number_cell_cell_id_cell_id_fk" FOREIGN KEY ("cell_id") REFERENCES "public"."cell"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "string_cell" ADD CONSTRAINT "string_cell_cell_id_cell_id_fk" FOREIGN KEY ("cell_id") REFERENCES "public"."cell"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "row_embedding_index" ON "row" USING hnsw ("row_embedding" vector_cosine_ops);
