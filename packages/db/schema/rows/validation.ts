9b90cc5f76f9cb4f83ced52f1a76596ca7fbe854
Nicholas Haley <hello@nichaley.com>
2025-06-29 16:19:44 -0400

    Add zod validation

--- packages/db/schema/rows/validation.ts
+++ packages/db/schema/rows/validation.ts
@@ -4,17 +4,23 @@ import {
   createUpdateSchema,
 } from "drizzle-zod";
 import { z } from "zod";
+import { blockSchema } from "@mapform/blocknote/validation";
 import { rows } from "./schema";
+
+const schemaExtension = {
+  description: blockSchema.array(),
+};
 
 /**
  * Rows
  */
-export const insertRowSchema = createInsertSchema(rows);
+export const insertRowSchema = createInsertSchema(rows, schemaExtension);
 
-export const selectRowSchema = createSelectSchema(rows);
+export const selectRowSchema = createSelectSchema(rows, schemaExtension);
 
 export const updateRowSchema = createUpdateSchema(rows, {
   id: z.string(),
+  ...schemaExtension,
 });
 
 export type InsertRow = z.infer<typeof insertRowSchema>;
