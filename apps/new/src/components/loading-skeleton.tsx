5cc0c88bd40ca9fe36e4d35330987fee7694bb20
Nicholas Haley <hello@nichaley.com>
2025-07-16 08:53:13 -0400

    wip

--- apps/new/src/components/loading-skeleton.tsx
+++ apps/new/src/components/loading-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@mapform/ui/components/skeleton";
 
 export function LoadingSkeleton() {
   return (
-    <div className="z-10 flex flex-col gap-2">
+    <div className="z-10 flex flex-col gap-2 p-6">
       <Skeleton className="size-8 rounded-full" />
       <Skeleton className="h-6 w-3/4" />
     </div>
