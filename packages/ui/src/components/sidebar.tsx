e5b87a4af48c29090df8c085d4b5848a983e8203
Nicholas Haley <hello@nichaley.com>
2025-08-18 21:17:57 -0400

    allow for shadcn install command

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
 
 import { cn } from "@mapform/lib/classnames";
-import { useIsMobile } from "../hooks/use-mobile";
+import { useIsMobile } from "../lib/hooks/use-mobile";
 import { Button } from "./button";
 import { Input } from "./input";
 import { Separator } from "./separator";
