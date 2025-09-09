8a20e71e2e641f28029ccd27a4943fd145bdd3f9
Nicholas Haley <hello@nichaley.com>
2025-09-08 22:50:16 -0400

    wip project markers

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/project-markers.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/project-markers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Marker } from "react-map-gl/mapbox";
+import { Marker, Popup } from "react-map-gl/mapbox";
 import { useWorkspace } from "./workspace-context";
 
 export function ProjectMarkers() {
@@ -10,10 +10,30 @@ export function ProjectMarkers() {
   );
 
   return projects.map((p) => (
-    <Marker
-      key={p.id}
-      longitude={p.center.coordinates[0]}
-      latitude={p.center.coordinates[1]}
-    />
+    // <Popup
+    //   key={p.id}
+    //   longitude={p.center.coordinates[0]}
+    //   latitude={p.center.coordinates[1]}
+    //   anchor="bottom"
+    //   // onClose={() => {}}
+    // >
+    //   {p.name ?? "Untitled Project"}
+    // </Popup>
+    <>
+      <Marker
+        key={p.id}
+        longitude={p.center.coordinates[0]}
+        latitude={p.center.coordinates[1]}
+      />
+      <Marker
+        key={p.id}
+        longitude={p.center.coordinates[0]}
+        latitude={p.center.coordinates[1]}
+      >
+        <div className="prose absolute bottom-0 left-0 h-28 w-40 -translate-x-1/2 rounded-md bg-white p-4 shadow-md">
+          {p.name ?? "New Map"}
+        </div>
+      </Marker>
+    </>
   ));
 }
