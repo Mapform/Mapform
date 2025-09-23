395d05044d88596ea975c1e7f9a4266e476afe5e
Nicholas Haley <hello@nichaley.com>
2025-09-23 14:28:37 -0400

    Improve lightbox loading animation

--- apps/app/src/components/image-lightbox/index.tsx
+++ apps/app/src/components/image-lightbox/index.tsx
@@ -9,6 +9,9 @@ import {
   DialogTrigger,
 } from "@mapform/ui/components/dialog";
 import { XIcon } from "lucide-react";
+import { Spinner } from "@mapform/ui/components/spinner";
+import { useEffect, useState } from "react";
+import { cn } from "@mapform/lib/classnames";
 
 interface ImageLightboxProps {
   activeImage: {
@@ -21,8 +24,22 @@ interface ImageLightboxProps {
 }
 
 export function ImageLightbox({ activeImage, children }: ImageLightboxProps) {
+  const [open, setOpen] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Reset loading state when the image changes
+    setIsLoaded(false);
+  }, [activeImage.url]);
+
   return (
-    <Dialog>
+    <Dialog
+      open={open}
+      onOpenChange={(value) => {
+        setOpen(value);
+        if (value) setIsLoaded(false);
+      }}
+    >
       <DialogTrigger className="cursor-pointer" asChild>
         {children}
       </DialogTrigger>
@@ -32,13 +49,27 @@ export function ImageLightbox({ activeImage, children }: ImageLightboxProps) {
           <XIcon className="size-5" />
           <span className="sr-only">Close</span>
         </DialogClose>
-        <DialogPrimitive.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 flex translate-x-[-50%] translate-y-[-50%] p-4 outline-none duration-200 sm:rounded-lg md:p-8">
+        <DialogPrimitive.Content
+          className={`data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] fixed left-[50%] top-[50%] z-50 flex translate-x-[-50%] translate-y-[-50%] p-4 outline-none duration-200 sm:rounded-lg md:p-8`}
+        >
           <div className="relative flex max-h-[calc(100vh-4rem)] max-w-[calc(100vw-4rem)] items-center justify-center overflow-hidden">
+            {!isLoaded && (
+              <div className="pointer-events-none absolute inset-0 z-[1] flex items-center justify-center">
+                <Spinner />
+              </div>
+            )}
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              className="m-0 max-h-full max-w-full rounded-md object-contain"
+              className={cn(
+                "m-0 max-h-full max-w-full rounded-md object-contain opacity-0 transition-opacity duration-200",
+                {
+                  "opacity-100": isLoaded,
+                },
+              )}
               src={activeImage.url}
               alt={activeImage.alt ?? ""}
+              onLoad={() => setIsLoaded(true)}
+              onError={() => setIsLoaded(true)}
             />
           </div>
         </DialogPrimitive.Content>
