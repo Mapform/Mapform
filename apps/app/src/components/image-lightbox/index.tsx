f4ae9c5c763484ba7530c200a59468948246a263
Nicholas Haley <hello@nichaley.com>
2025-09-23 11:28:06 -0400

    Styling lightbox

--- apps/app/src/components/image-lightbox/index.tsx
+++ apps/app/src/components/image-lightbox/index.tsx
@@ -10,7 +10,6 @@ import {
   DialogTrigger,
 } from "@mapform/ui/components/dialog";
 import { ChevronLeftIcon, ChevronRightIcon, XIcon } from "lucide-react";
-import Image from "next/image";
 
 interface ImageLightboxProps {
   activeImage: {
@@ -37,18 +36,17 @@ export function ImageLightbox({
       </DialogTrigger>
       <DialogPortal>
         <DialogOverlay />
-        <DialogPrimitive.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 flex size-full w-full translate-x-[-50%] translate-y-[-50%] p-8 duration-200 sm:rounded-lg">
-          <DialogClose className="absolute right-4 top-4 z-[60] text-white">
-            <XIcon className="size-5" />
-            <span className="sr-only">Close</span>
-          </DialogClose>
-          <div className="relative size-full flex-1 overflow-hidden">
-            <Image
-              className="m-0 rounded-md"
+        <DialogClose className="pointer-events-auto fixed right-4 top-4 z-[9999] text-white">
+          <XIcon className="size-5" />
+          <span className="sr-only">Close</span>
+        </DialogClose>
+        <DialogPrimitive.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 flex translate-x-[-50%] translate-y-[-50%] p-4 duration-200 sm:rounded-lg md:p-8">
+          <div className="relative flex max-h-[calc(100vh-4rem)] max-w-[calc(100vw-4rem)] items-center justify-center overflow-hidden">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="m-0 max-h-full max-w-full rounded-md object-contain"
               src={activeImage.url}
               alt={activeImage.alt ?? ""}
-              fill
-              objectFit="contain"
             />
           </div>
           {onPrevious && (
