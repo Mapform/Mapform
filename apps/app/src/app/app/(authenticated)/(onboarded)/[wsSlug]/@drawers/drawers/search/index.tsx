51a1ffe6504f1d860bb7e583a1a0306815295c47
Nicholas Haley <hello@nichaley.com>
2025-09-28 23:51:12 -0400

    wip - restyling search field

--- apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/@drawers/drawers/search/index.tsx
+++ apps/app/src/app/app/(authenticated)/(onboarded)/[wsSlug]/@drawers/drawers/search/index.tsx
@@ -12,7 +12,9 @@ import {
   GlobeIcon,
   Loader2,
   MessageCircle,
+  PlusIcon,
   SearchIcon,
+  SendIcon,
   TrashIcon,
   XIcon,
 } from "lucide-react";
@@ -74,31 +76,48 @@ export function Search({
       <Command className="bg-transparent" shouldFilter={false}>
         <MapDrawerToolbar className="border-b max-md:pt-6">
           <div
-            className="hover:bg-muted focus-within:ring-ring focus-within:bg-muted relative flex flex-1 items-center rounded-md pl-3 pr-1 transition-all focus-within:ring-2"
+            className="relative flex flex-1 flex-col gap-2 rounded-md"
             cmdk-input-wrapper=""
           >
-            {isPending && query && query.length > 0 ? (
-              <Loader2 className="mr-2 h-4 w-4 shrink-0 animate-spin opacity-50" />
-            ) : (
-              <SearchIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
-            )}
-            <CommandPrimitive.Input
-              className="placeholder:text-muted-foreground flex h-9 w-full rounded-md border-none bg-transparent px-1 py-3 pr-8 text-base outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-              placeholder="Search or ask..."
-              onValueChange={setQuery}
-              value={query ?? ""}
-              autoFocus
-            />
-            <Button
-              className="absolute right-0 top-0"
-              variant="ghost"
-              size="icon"
-              onClick={() => {
-                void setQueryStates({ search: null, query: null });
-              }}
-            >
-              <XIcon className="size-4" />
-            </Button>
+            <div className="flex flex-1 items-center">
+              {isPending && query && query.length > 0 ? (
+                <Loader2 className="mr-2 h-4 w-4 shrink-0 animate-spin opacity-50" />
+              ) : (
+                <SearchIcon className="mr-2 size-4 shrink-0 opacity-50" />
+              )}
+              <CommandPrimitive.Input
+                className="placeholder:text-muted-foreground flex h-9 w-full rounded-md border-none bg-transparent py-3 pl-0 pr-8 !text-base outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                placeholder="Search or ask..."
+                onValueChange={setQuery}
+                value={query ?? ""}
+                autoFocus
+              />
+              <Button
+                className="absolute right-0 top-0"
+                variant="ghost"
+                size="icon"
+                onClick={() => {
+                  void setQueryStates({ search: null, query: null });
+                }}
+              >
+                <XIcon className="size-4" />
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button
+                className="text-muted-foreground !p-0"
+                style={{
+                  background: "none",
+                }}
+                variant="ghost"
+              >
+                <PlusIcon className="size-4" />
+                Add Context
+              </Button>
+              <Button className="ml-auto" type="submit" size="icon">
+                <SendIcon className="size-4" />
+              </Button>
+            </div>
           </div>
         </MapDrawerToolbar>
         <CommandList className="max-h-full p-2">
