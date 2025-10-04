e4abf0ddbfc55b7cd25ee71995a3c34301774845
Nicholas Haley <hello@nichaley.com>
2025-10-04 14:38:47 -0400

    Add playsinline to video

--- apps/app/src/components/tour-guide.tsx
+++ apps/app/src/components/tour-guide.tsx
@@ -107,6 +107,7 @@ export function TourContent({ steps }: TourGuideProps) {
                   muted
                   loop
                   controls={false}
+                  playsInline
                 />
               )}
             </div>
